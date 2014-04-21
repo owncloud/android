800b04d5207e6d5d81c3fa9220b0c6ccd0427fcf
David A. Velasco <dvelasco@solidgear.es>
2014-04-21 11:14:51 +0200

    Trying to wait more time for the emulator

--- wait_for_emulator.sh
+++ wait_for_emulator.sh
@@ -2,16 +2,18 @@
 
 bootanim=""
 failcounter=0
+checkcounter=0
 until [[ "$bootanim" =~ "stopped" ]]; do
    bootanim=`adb -e shell getprop init.svc.bootanim 2>&1`
-   echo "$bootanim"
+   echo "($checkcounter) $bootanim"
    if [[ "$bootanim" =~ "not found" ]]; then
       let "failcounter += 1"
-      if [[ $failcounter -gt 3 ]]; then
+      if [[ $failcounter -gt 300 ]]; then
         echo "Failed to start emulator"
         exit 1
       fi
    fi
+   let "checkcounter += 1"
    sleep 1
 done
 echo "Done"
