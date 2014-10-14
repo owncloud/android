b2935cbd937dfaa511584929924c58c92f7d12c5
MMMarcy <steiner.marcello@gmail.com>
2014-10-14 21:29:14 +0200

    Fixed problems introduced when updating with the original repository. Now it builds on gradle, maven and ant on linux. Also I updated the setup_env.sh, but there is still room for improvement.
    Finally updated the .gitignore to not track ./androidbarsherlock
    subdirectory

--- setup_env.sh
+++ setup_env.sh
@@ -15,13 +15,17 @@ function initDefault {
 function initForAnt {
     if [ ! -d "$DIRECTORY" ]; then
 
-        mkdir actionbarsherlock
-        git submodule add --force git://github.com/JakeWharton/ActionBarSherlock.git ./actionbarsherlock
+        #Gets the owncloud-android-library
         git submodule init
         git submodule update
+
+        #Clones the actionbarsherlock and checks-out the right release (4.1.0)
+        git clone git://github.com/JakeWharton/ActionBarSherlock.git actionbarsherlock
         cd actionbarsherlock/
         git checkout 9598f2b
         cd ../
+
+        #As default it updates the ant scripts
         android update project -p actionbarsherlock/library -n ActionBarSherlock
         android update lib-project -p owncloud-android-library
         android update project -p .
