33e2a2e9c37c3cfba1e19efbfda1bf851bcc6c4f
MMMarcy <steiner.marcello@gmail.com>
2014-10-14 12:21:41 +0200

    Fixed the scripFixed the script

--- setup_env.sh
+++ setup_env.sh
@@ -14,14 +14,19 @@ function initDefault {
 
 function initForAnt {
     if [ ! -d "$DIRECTORY" ]; then
-        git submodule add git://github.com/JakeWharton/ActionBarSherlock.git --name actionbarsherlock
+
+        mkdir actionbarsherlock
+        git submodule add --force git://github.com/JakeWharton/ActionBarSherlock.git ./actionbarsherlock
         git submodule init
         git submodule update
+        cd actionbarsherlock/
+        git checkout 9598f2b
+        cd ../
         android update project -p actionbarsherlock/library -n ActionBarSherlock
         android update lib-project -p owncloud-android-library
         android update project -p .
         android update project -p oc_jb_workaround
-        #cp third_party/android-support-library/android-support-v4.jar actionbarsherlock/library/libs/android-support-v4.jar
+        cp third_party/android-support-library/android-support-v4.jar actionbarsherlock/library/libs/android-support-v4.jar
         android update test-project -p tests -m ..
     fi
 }
@@ -30,6 +35,7 @@ function initForAnt {
               initDefault
               exit
  else
+              echo "Creating environment for Ant"
               initForAnt
               exit
  fi
