6f1232493087fc70dfe24e97a92cfe88c731107e
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2014-11-04 16:31:12 +0100

    Added some missing imports

--- test_client/tests/src/com/owncloud/android/lib/test_project/test/CopyFileTest.java
+++ test_client/tests/src/com/owncloud/android/lib/test_project/test/CopyFileTest.java
@@ -36,11 +36,18 @@ import com.owncloud.android.lib.common.network.NetworkUtils;
 import com.owncloud.android.lib.common.operations.RemoteOperationResult;
 import com.owncloud.android.lib.common.operations.RemoteOperationResult.ResultCode;
 import com.owncloud.android.lib.resources.files.CopyRemoteFileOperation;
+import com.owncloud.android.lib.test_project.R;
+import com.owncloud.android.lib.test_project.SelfSignedConfidentSslSocketFactory;
 import com.owncloud.android.lib.test_project.TestActivity;
 
+import junit.framework.AssertionFailedError;
+
 import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.protocol.Protocol;
+import org.apache.commons.httpclient.protocol.ProtocolSocketFactory;
 
 import java.io.File;
+import java.security.GeneralSecurityException;
 
 //import android.test.AndroidTestCase;
 
