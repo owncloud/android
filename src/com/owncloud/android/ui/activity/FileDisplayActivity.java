26ec623571f42147c169cbe4bf71e37b744bf35b
masensio <masensio@solidgear.es>
2015-07-09 14:05:31 +0200

    New folder from shortcuts widget

--- src/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ src/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -128,6 +128,8 @@ public class FileDisplayActivity extends HookActivity
 
     public static final String EXTRA_UPLOAD_FROM_WIDGET =
             "com.owncloud.android.ui.activity.UPLOAD_FROM_WIDGET";
+    public static final String EXTRA_NEW_FROM_WIDGET =
+            "com.owncloud.android.ui.activity.NEW_FROM_WIDGET";
 
     public static final String ACTION_DETAILS = "com.owncloud.android.ui.activity.action.DETAILS";
 
@@ -152,6 +154,7 @@ public class FileDisplayActivity extends HookActivity
     private OCFile mWaitingToSend;
 
     private boolean mUploadFromWidget = false;
+    private boolean mNewFromWidget = false;
 
     
     @Override
@@ -182,6 +185,8 @@ public class FileDisplayActivity extends HookActivity
 
             mUploadFromWidget = getIntent().getBooleanExtra(
                     FileDisplayActivity.EXTRA_UPLOAD_FROM_WIDGET, false);
+            mNewFromWidget = getIntent().getBooleanExtra(
+                    FileDisplayActivity.EXTRA_NEW_FROM_WIDGET, false);
         }        
 
         /// USER INTERFACE
@@ -224,6 +229,11 @@ public class FileDisplayActivity extends HookActivity
             UploadSourceDialogFragment dialog =
                     UploadSourceDialogFragment.newInstance(getAccount());
             dialog.show(getSupportFragmentManager(), DIALOG_UPLOAD_SOURCE);
+        }
+        if (mNewFromWidget) {
+            CreateFolderDialogFragment dialog =
+                    CreateFolderDialogFragment.newInstance(getCurrentDir());
+            dialog.show(getSupportFragmentManager(), DIALOG_CREATE_FOLDER);
         }
 
         Log_OC.v(TAG, "onStart() end");
