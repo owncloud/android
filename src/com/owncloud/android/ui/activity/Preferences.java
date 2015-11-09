ed740e276633fde189358c8f722c71ec79ae8e0a
Bartosz Przybylski <bart.p.pl@gmail.com>
2015-11-09 22:06:53 +0100

    Fix after rebase

--- src/com/owncloud/android/ui/activity/Preferences.java
+++ src/com/owncloud/android/ui/activity/Preferences.java
@@ -381,7 +381,7 @@ public class Preferences extends PreferenceActivity
             }
         }
 
-        mPrefStoragePath =  findPreference("storage_path");
+        mPrefStoragePath =  (PreferenceWithLongSummary)findPreference("storage_path");
         if (mPrefStoragePath != null) {
 
             mPrefStoragePath.setOnPreferenceClickListener(new OnPreferenceClickListener() {
@@ -403,7 +403,7 @@ public class Preferences extends PreferenceActivity
                 });
         }
 
-        mPrefInstantUploadPath =  findPreference("instant_upload_path");
+        mPrefInstantUploadPath = (PreferenceWithLongSummary)findPreference("instant_upload_path");
         if (mPrefInstantUploadPath != null){
 
             mPrefInstantUploadPath.setOnPreferenceClickListener(new OnPreferenceClickListener() {
