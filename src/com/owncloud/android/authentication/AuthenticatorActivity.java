9798899264b63e8998dd938fa9190860bcf63035
David A. Velasco <dvelasco@owncloud.com>
2017-01-25 17:56:25 +0100

    Fix log into server version 10

--- src/com/owncloud/android/authentication/AuthenticatorActivity.java
+++ src/com/owncloud/android/authentication/AuthenticatorActivity.java
@@ -667,12 +667,11 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
 
             OwnCloudCredentials credentials = null;
             if (BASIC_TOKEN_TYPE.equals(mAuthTokenType)) {
-                String version = savedInstanceState.getString(KEY_OC_VERSION);
-                OwnCloudVersion ocVersion = (version != null) ? new OwnCloudVersion(version) : null;
                 credentials = OwnCloudCredentialsFactory.newBasicCredentials(
                     username,
                     password,
-                    (ocVersion != null && ocVersion.isSessionMonitoringSupported())
+                    false // currently session is not created on log-in, but in first request after
+                          // TODO create session for Basic-Auth on log-in
                 );
 
             } else if (OAUTH_TOKEN_TYPE.equals(mAuthTokenType)) {
@@ -973,10 +972,8 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
         OwnCloudCredentials credentials = OwnCloudCredentialsFactory.newBasicCredentials(
             username,
             password,
-            (mServerInfo != null &&
-                mServerInfo.mVersion != null &&
-                mServerInfo.mVersion.isSessionMonitoringSupported()
-            )
+            false       // currently session is not created on log-in, but in first request after
+                        // TODO create session for Basic-Auth on log-in
         );
         accessRootFolder(credentials);
     }
