8871ca51300b4515e43208392d33ca8ccf2f89bf
David A. Velasco <dvelasco@owncloud.com>
2017-04-12 11:17:24 +0200

    Prevent crash in scheduled retry of upload that was deleted by the user

--- src/com/owncloud/android/files/services/RetryUploadJobService.java
+++ src/com/owncloud/android/files/services/RetryUploadJobService.java
@@ -8,9 +8,12 @@ import android.support.annotation.RequiresApi;
 import com.owncloud.android.datamodel.OCFile;
 import com.owncloud.android.datamodel.UploadsStorageManager;
 import com.owncloud.android.db.OCUpload;
+import com.owncloud.android.lib.common.utils.Log_OC;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class RetryUploadJobService extends JobService {
+
+    private static final String TAG = RetryUploadJobService.class.getName();
 
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
@@ -21,13 +24,25 @@ public class RetryUploadJobService extends JobService {
 
         String accountName = jobParameters.getExtras().getString(FileUploader.EXTRA_ACCOUNT_NAME);
 
+        Log_OC.d(TAG, String.format("Retrying upload of %1s in %2s", fileRemotePath, accountName));
+
         // Get upload to be retried
         OCUpload ocUpload = uploadsStorageManager.getLastUploadFor(new OCFile(fileRemotePath), accountName);
 
-        FileUploader.UploadRequester uploadRequester = new FileUploader.UploadRequester();
-
-        // Retry the upload
-        uploadRequester.retry(this, ocUpload);
+        if (ocUpload != null) {
+            // Retry the upload
+            FileUploader.UploadRequester uploadRequester = new FileUploader.UploadRequester();
+            uploadRequester.retry(this, ocUpload);
+        } else {
+            // easy if the user deletes the upload in uploads view before recovering network
+            Log_OC.w(
+                TAG,
+                String.format(
+                    "No upload found in database for %1s in %2s",
+                    fileRemotePath, accountName
+                )
+            );
+        }
 
         return true;
     }
