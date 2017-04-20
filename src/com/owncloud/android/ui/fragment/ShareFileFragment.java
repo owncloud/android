af99bd1dd546517ecfa02cab1b30fd1eb8a3be8e
davigonz <dgonzalez@solidgear.es>
2017-04-20 13:52:29 +0200

    Handle password toggle behavior when creating a public link

--- src/com/owncloud/android/ui/fragment/ShareFileFragment.java
+++ src/com/owncloud/android/ui/fragment/ShareFileFragment.java
@@ -32,7 +32,6 @@ import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ImageView;
