4dacb0a83e4c745cb87094f0bfa40c86835dfefa
davigonz <dgonzalez@solidgear.es>
2017-04-21 14:28:44 +0200

    Fix not showing public link name

--- src/com/owncloud/android/ui/activity/ShareActivity.java
+++ src/com/owncloud/android/ui/activity/ShareActivity.java
@@ -262,7 +262,6 @@ public class ShareActivity extends FileActivity
                 && getEditShareFragment() != null && getEditShareFragment().isAdded()) {
             getEditShareFragment().onUpdateSharePermissionsFinished(result);
         }
-
     }
 
 
