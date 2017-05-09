beb9be05de7a29c5bd6599a137b3544477fd6086
davigonz <dgonzalez@solidgear.es>
2017-05-09 09:05:21 +0200

    Fix hide + button to create a public share in servers <10

--- src/com/owncloud/android/lib/resources/status/OwnCloudVersion.java
+++ src/com/owncloud/android/lib/resources/status/OwnCloudVersion.java
@@ -48,7 +48,7 @@ public class OwnCloudVersion implements Comparable<OwnCloudVersion> {
     private static final int MINIMUM_VERSION_WITH_SESSION_MONITORING_WORKING_IN_PREEMPTIVE_MODE = 0x09010301;
     // 9.1.3.1, final 9.1.3: https://github.com/owncloud/core/commit/f9a867b70c217463289a741d4d26079eb2a80dfd
 
-    private static final int MINIMUM_VERSION_WITH_MULTIPLE_PUBLIC_SHARING = 0xA0000000;
+    private static final int MINIMUM_VERSION_WITH_MULTIPLE_PUBLIC_SHARING = 0xA000000;
 
     private static final String INVALID_ZERO_VERSION = "0.0.0";
 
