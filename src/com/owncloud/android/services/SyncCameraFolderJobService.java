8cba7d0c78bc19767714d2a8ad60486ea9fa2761
davigonz <dgonzalez@solidgear.es>
2017-11-07 17:53:00 +0100

    Renaming variables

--- src/com/owncloud/android/services/SyncCameraFolderJobService.java
+++ src/com/owncloud/android/services/SyncCameraFolderJobService.java
@@ -210,6 +210,7 @@ public class SyncCameraFolderJobService extends JobService implements OnRemoteOp
 
                     handleNewFile(localFile);
                 }
+
             }
 
         } else {
