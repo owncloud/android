5508c87a7fb3b5c12188db98cb235d92e090d74c
sd1998 <shashvat51@gmail.com>
2018-02-12 16:41:18 +0530

    Code review fixes

--- src/com/owncloud/android/authentication/PatternManager.java
+++ src/com/owncloud/android/authentication/PatternManager.java
@@ -1,3 +1,23 @@
+/**
+ *   ownCloud Android client application
+ *
+ *   @author Shashvat Kedia
+ *   Copyright (C) 2018 ownCloud GmbH.
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   as published by the Free Software Foundation.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.owncloud.android.authentication;
 
 import android.app.Activity;
@@ -14,10 +34,6 @@ import com.owncloud.android.ui.activity.PatternLockActivity;
 
 import java.util.HashSet;
 import java.util.Set;
-
-/**
- * Created by shashvatkedia on 21/12/17.
- */
 
 public class PatternManager{
 
