26edad61dd1add7d85c484e4517a3fee29e9e5fd
davigonz <dgonzalez@solidgear.es>
2018-11-16 08:58:08 +0100

    Show wizard when upgrading to 2.9.2

--- src/com/owncloud/android/features/FeatureList.java
+++ src/com/owncloud/android/features/FeatureList.java
@@ -59,11 +59,11 @@ public class FeatureList {
 
             // Features introduced in 2.9.1
             new FeatureItem(R.drawable.whats_new_current_folder_search, R.string.welcome_feature_6_title,
-                    R.string.welcome_feature_6_text, "2.9.1", "0", SHOW_ON_UPGRADE),
+                    R.string.welcome_feature_6_text, "2.9.2", "0", SHOW_ON_UPGRADE),
             new FeatureItem(R.drawable.whats_new_select_all, R.string.welcome_feature_7_title,
-                    R.string.welcome_feature_7_text, "2.9.1", "0", SHOW_ON_UPGRADE),
+                    R.string.welcome_feature_7_text, "2.9.2", "0", SHOW_ON_UPGRADE),
             new FeatureItem(R.drawable.whats_new_av_offline_jobs, R.string.welcome_feature_8_title,
-                    R.string.welcome_feature_8_text, "2.9.1", "0", SHOW_ON_UPGRADE)
+                    R.string.welcome_feature_8_text, "2.9.2", "0", SHOW_ON_UPGRADE)
     };
 
 
