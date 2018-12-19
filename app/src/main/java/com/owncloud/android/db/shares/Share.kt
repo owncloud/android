9195d9d591bc12d23f0534b6855b593c325d56d7
davigonz <dgonzalez@solidgear.es>
2018-12-19 14:08:09 +0100

    Include Share entity, ShareDao with query to get public shares for a file and LiveData

---
+++ app/src/main/java/com/owncloud/android/db/shares/Share.kt
@@ -0,0 +1,26 @@
+package com.owncloud.android.db.shares
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+import com.owncloud.android.lib.resources.shares.ShareType
+
+@Entity(tableName = "shares_table")
+data class Share(
+        @PrimaryKey val id: Long,
+        val fileSource: Long,
+        val itemSource: Long,
+        val type: ShareType,
+        val shareWith: String,
+        val path: String,
+        val permissions: Int,
+        val sharedDate: Long,
+        val expirationDate: Long,
+        val token: String,
+        val sharedWithDisplayName: String,
+        val name: String,
+        val isFolder: Boolean,
+        val userId: Long,
+        val remoteId: Long,
+        val shareLink: String,
+        val accountOwner: String
+)
