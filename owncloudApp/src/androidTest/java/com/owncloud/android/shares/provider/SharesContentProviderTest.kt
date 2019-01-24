ebae3740a009678ce9ebc677eab9800fa7731a92
davigonz <dgonzalez@solidgear.es>
2019-01-24 15:25:48 +0100

    Initial tests for OCShareDao and OCLocalSharesDataSource and some renaming

--- owncloudApp/src/androidTest/java/com/owncloud/android/shares/provider/SharesContentProviderTest.kt
+++ owncloudApp/src/androidTest/java/com/owncloud/android/shares/provider/SharesContentProviderTest.kt
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.owncloud.android.provider
+package com.owncloud.android.shares.provider
 
 import android.content.ContentResolver
 import android.content.ContentValues
