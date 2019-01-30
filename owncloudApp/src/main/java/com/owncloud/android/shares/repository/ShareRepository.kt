a62d3657522660a0619e03b0ffd9b9d53914857e
davigonz <dgonzalez@solidgear.es>
2019-01-30 17:41:50 +0100

    Include OCShareDaoTest

--- owncloudApp/src/main/java/com/owncloud/android/shares/repository/ShareRepository.kt
+++ owncloudApp/src/main/java/com/owncloud/android/shares/repository/ShareRepository.kt
@@ -20,10 +20,7 @@
 package com.owncloud.android.shares.repository
 
 import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
 import com.owncloud.android.Resource
-import com.owncloud.android.lib.common.operations.RemoteOperationResult
-import com.owncloud.android.lib.resources.shares.ShareParserResult
 import com.owncloud.android.lib.resources.shares.ShareType
 import com.owncloud.android.shares.db.OCShare
 
