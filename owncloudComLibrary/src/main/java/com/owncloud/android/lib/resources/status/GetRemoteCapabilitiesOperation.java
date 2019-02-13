78e4d167d2ace375878af179d1767ea876540e49
Hannes Achleitner <hannes.software@gmx.at>
2019-02-13 06:52:48 +0100

    change 2018 to 2019 in comments

--- owncloudComLibrary/src/main/java/com/owncloud/android/lib/resources/status/GetRemoteCapabilitiesOperation.java
+++ owncloudComLibrary/src/main/java/com/owncloud/android/lib/resources/status/GetRemoteCapabilitiesOperation.java
@@ -1,7 +1,7 @@
 /* ownCloud Android Library is available under MIT license
  *   @author masensio
  *   @author Semih Serhat Karakaya <karakayasemi@itu.edu.tr>
- *   Copyright (C) 2018 ownCloud GmbH.
+ *   Copyright (C) 2019 ownCloud GmbH.
  *
  *   Permission is hereby granted, free of charge, to any person obtaining a copy
  *   of this software and associated documentation files (the "Software"), to deal
