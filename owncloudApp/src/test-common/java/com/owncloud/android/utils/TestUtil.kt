54cbc6e91d25ea66db3b8608718ae9baa7913b7a
davigonz <dgonzalez@solidgear.es>
2019-03-19 10:07:51 +0100

    Add tests for insert shares in OCShareRepository and clean up some of them

--- owncloudApp/src/test-common/java/com/owncloud/android/utils/TestUtil.kt
+++ owncloudApp/src/test-common/java/com/owncloud/android/utils/TestUtil.kt
@@ -20,81 +20,83 @@
 package com.owncloud.android.utils
 
 import android.accounts.Account
-import android.hardware.SensorAdditionalInfo
+import com.owncloud.android.lib.common.operations.RemoteOperationResult
 import com.owncloud.android.lib.resources.shares.RemoteShare
 import com.owncloud.android.lib.resources.shares.ShareType
 import com.owncloud.android.shares.db.OCShare
+import org.mockito.Mockito.`when`
+import java.lang.Exception
 
 object TestUtil {
     private fun createShare(
-            fileSource: Long = 7,
-            itemSource: Long = 7,
-            shareType: Int, // Public share by default
-            shareWith: String = "",
-            path: String,
-            permissions: Int = 1,
-            sharedDate: Long = 1542628397,
-            expirationDate: Long = 0,
-            token: String = "pwdasd12dasdWZ",
-            sharedWithDisplayName: String = "",
-            sharedWithAdditionalInfo: String = "",
-            isFolder: Boolean,
-            userId: Long = -1,
-            remoteId: Long = 1,
-            accountOwner: String = "admin@server",
-            name: String,
-            shareLink: String
+        fileSource: Long = 7,
+        itemSource: Long = 7,
+        shareType: Int, // Public share by default
+        shareWith: String = "",
+        path: String,
+        permissions: Int = 1,
+        sharedDate: Long = 1542628397,
+        expirationDate: Long = 0,
+        token: String = "pwdasd12dasdWZ",
+        sharedWithDisplayName: String = "",
+        sharedWithAdditionalInfo: String = "",
+        isFolder: Boolean,
+        userId: Long = -1,
+        remoteId: Long = 1,
+        accountOwner: String = "admin@server",
+        name: String,
+        shareLink: String
     ) = OCShare(
-            fileSource,
-            itemSource,
-            shareType,
-            shareWith,
-            path,
-            permissions,
-            sharedDate,
-            expirationDate,
-            token,
-            sharedWithDisplayName,
-            sharedWithAdditionalInfo,
-            isFolder,
-            userId,
-            remoteId,
-            accountOwner,
-            name,
-            shareLink
+        fileSource,
+        itemSource,
+        shareType,
+        shareWith,
+        path,
+        permissions,
+        sharedDate,
+        expirationDate,
+        token,
+        sharedWithDisplayName,
+        sharedWithAdditionalInfo,
+        isFolder,
+        userId,
+        remoteId,
+        accountOwner,
+        name,
+        shareLink
     )
 
     fun createPublicShare(
-            path: String,
-            isFolder: Boolean,
-            accountOwner: String = "admin@server",
-            name: String,
-            shareLink: String
+        path: String,
+        isFolder: Boolean,
+        accountOwner: String = "admin@server",
+        name: String,
+        shareLink: String
     ) = createShare(
-            shareType = 3,
-            path = path,
-            isFolder = isFolder,
-            accountOwner = accountOwner,
-            name = name,
-            shareLink = shareLink
+        shareType = 3,
+        path = path,
+        isFolder = isFolder,
+        accountOwner = accountOwner,
+        name = name,
+        shareLink = shareLink
     )
 
     fun createRemoteShare(
-            fileSource: Long = 7,
-            itemSource: Long = 7,
-            shareType: Int = 3, // Public share by default
-            shareWith: String = "",
-            path: String,
-            permissions: Int = 1,
-            sharedDate: Long = 1542628397,
-            expirationDate: Long = 0,
-            token: String = "pwdasd12dasdWZ",
-            sharedWithDisplayName: String = "",
-            isFolder: Boolean,
-            userId: Long = -1,
-            remoteId: Long = 1,
-            name: String,
-            shareLink: String
+        fileSource: Long = 7,
+        itemSource: Long = 7,
+        shareType: Int = 3, // Public share by default
+        shareWith: String = "",
+        path: String,
+        permissions: Int = 1,
+        sharedDate: Long = 1542628397,
+        expirationDate: Long = 0,
+        token: String = "pwdasd12dasdWZ",
+        sharedWithDisplayName: String = "",
+        isFolder: Boolean,
+        userId: Long = -1,
+        remoteId: Long = 1,
+        name: String,
+        shareLink: String
     ): RemoteShare {
         val remoteShare = RemoteShare();
 
@@ -128,5 +130,32 @@ object TestUtil {
         typeField.set(account, type)
 
         return account
+    }
+
+    fun <T> createRemoteOperationResultMock(
+        data: T,
+        isSuccess: Boolean,
+        httpPhrase: String? = null,
+        resultCode: RemoteOperationResult.ResultCode? = null,
+        exception: Exception? = null
+    ): RemoteOperationResult<T> {
+        val remoteOperationResult = mock<RemoteOperationResult<T>>()
+
+        `when`(remoteOperationResult.data).thenReturn(data)
+        `when`(remoteOperationResult.isSuccess).thenReturn(isSuccess)
+
+        if (httpPhrase != null) {
+            `when`(remoteOperationResult.httpPhrase).thenReturn(httpPhrase)
+        }
+
+        if (resultCode != null) {
+            `when`(remoteOperationResult.code).thenReturn(resultCode)
+        }
+
+        if (exception != null) {
+            `when`(remoteOperationResult.exception).thenReturn(exception)
+        }
+
+        return remoteOperationResult
     }
 }
