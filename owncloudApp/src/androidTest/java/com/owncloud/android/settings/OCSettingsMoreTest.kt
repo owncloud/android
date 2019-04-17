dd871ca4415595eff79a03fd5ef39853d7470cb1
jrecio <jrecio@solidgear.es>
2019-04-17 16:03:40 +0200

    create UI tests for about section; polish and order code

--- owncloudApp/src/androidTest/java/com/owncloud/android/settings/OCSettingsMoreTest.kt
+++ owncloudApp/src/androidTest/java/com/owncloud/android/settings/OCSettingsMoreTest.kt
@@ -19,45 +19,38 @@
 
 package com.owncloud.android.settings
 
-import android.preference.CheckBoxPreference
+import android.app.Activity
+import android.app.Instrumentation.ActivityResult
+import android.content.Intent
+import android.net.Uri
 import android.preference.PreferenceCategory
 import android.preference.PreferenceScreen
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.action.ViewActions.scrollTo
-import androidx.test.espresso.matcher.ViewMatchers.isClickable
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.ActivityTestRule
-import com.owncloud.android.R
-import com.owncloud.android.ui.activity.Preferences
-import org.junit.Before
-import org.junit.Rule
-import org.junit.runner.RunWith
-import org.junit.Test
-import org.hamcrest.Matchers.not
-import org.hamcrest.Matchers.allOf
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.Intents.intending
-import android.content.Intent
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.intent.matcher.IntentMatchers.anyIntent
-import android.app.Instrumentation.ActivityResult
-import android.app.Activity
-import android.net.Uri
-import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasFlag
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import com.owncloud.android.BuildConfig
+import com.owncloud.android.R
 import com.owncloud.android.ui.activity.LogHistoryActivity
+import com.owncloud.android.ui.activity.Preferences
 import com.owncloud.android.ui.activity.PrivacyPolicyActivity
+import org.hamcrest.Matchers.allOf
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class OCSettingsMoreTest {
@@ -75,6 +68,14 @@ class OCSettingsMoreTest {
         val securityCategory = activityRule.activity.findPreference("security_category") as PreferenceCategory
         preferenceScreen.removePreference(cameraUploadsCategory)
         preferenceScreen.removePreference(securityCategory)
+
+        //Starts the intent catching
+        Intents.init()
+    }
+
+    @After
+    fun tearDown(){
+        Intents.release()
     }
 
 
@@ -84,13 +85,12 @@ class OCSettingsMoreTest {
     }
 
     @Test
-    fun helpOptionOpensWeb(){
-        Intents.init()
-        onView(withText(R.string.prefs_help)).perform(click())
+    fun helpOpensWeb(){
         val intentResult = ActivityResult(Activity.RESULT_OK, Intent())
+        val helpUrl = activityRule.activity.getText(R.string.url_help) as String
         intending(hasAction(Intent.ACTION_VIEW)).respondWith(intentResult);
-        intended(hasData("https://owncloud.com/mobile/help"))
-        Intents.release()
+        onView(withText(R.string.prefs_help)).perform(click())
+        intended(hasData(helpUrl))
     }
 
     @Test
@@ -101,12 +101,10 @@ class OCSettingsMoreTest {
 
     @Test
     fun davx5OpensLink(){
-        Intents.init()
         val intentResult = ActivityResult(Activity.RESULT_OK, Intent())
         intending(hasAction(Intent.ACTION_VIEW)).respondWith(intentResult);
         onView(withText(R.string.prefs_sync_calendar_contacts)).perform(click())
-        intended(hasData("https://play.google.com/store/apps/details?id=at.bitfire.davdroid"))
-        Intents.release()
+        intended(hasData(activityRule.activity.getText(R.string.url_sync_calendar_contacts) as String))
     }
 
     @Test
@@ -116,7 +114,6 @@ class OCSettingsMoreTest {
 
     @Test
     fun recommendOpenSender(){
-        Intents.init()
         val intentResult = ActivityResult(Activity.RESULT_OK, Intent())
         intending(hasAction(Intent.ACTION_SENDTO)).respondWith(intentResult);
         onView(withText(R.string.prefs_recommend)).perform(click())
@@ -133,7 +130,6 @@ class OCSettingsMoreTest {
                     activityRule.activity.getString(R.string.url_app_download)
                 )),
             hasFlag(Intent.FLAG_ACTIVITY_NEW_TASK)))
-        Intents.release()
     }
 
     @Test
@@ -143,7 +139,6 @@ class OCSettingsMoreTest {
 
     @Test
     fun feedbackOpenSender(){
-        Intents.init()
         val intentResult = ActivityResult(Activity.RESULT_OK, Intent())
         intending(hasAction(Intent.ACTION_SENDTO)).respondWith(intentResult);
         onView(withText(R.string.drawer_feedback)).perform(click())
@@ -153,7 +148,6 @@ class OCSettingsMoreTest {
                 "Android v" + BuildConfig.VERSION_NAME + " - " + activityRule.activity.getText(R.string.prefs_feedback)),
             hasData(Uri.parse(activityRule.activity.getText(R.string.mail_feedback) as String)),
             hasFlag(Intent.FLAG_ACTIVITY_NEW_TASK)))
-        Intents.release()
     }
 
     @Test
@@ -162,11 +156,9 @@ class OCSettingsMoreTest {
     }
 
     @Test
-    fun loggerOpen(){
-        Intents.init()
+    fun loggerOpenView(){
         onView(withText(R.string.actionbar_logger)).perform(click())
         intended(hasComponent(LogHistoryActivity::class.java.name))
-        Intents.release()
     }
 
     @Test
@@ -176,10 +168,42 @@ class OCSettingsMoreTest {
 
     @Test
     fun privacyPolicyOpenWeb(){
-        Intents.init()
         onView(withText(R.string.prefs_privacy_policy)).perform(click())
         intended(hasComponent(PrivacyPolicyActivity::class.java.name))
-        Intents.release()
     }
+
+    @Test
+    fun aboutView(){
+        val version = activityRule.activity.getPackageManager()
+            .getPackageInfo(activityRule.activity.getPackageName(), 0).versionName
+        val commit = BuildConfig.COMMIT_SHA1
+        val type = BuildConfig.BUILD_TYPE
+        onView(withText(
+                String.format(
+                    activityRule.activity.getString(R.string.about_android),
+                    activityRule.activity.getString(R.string.app_name))))
+            .check(matches(isDisplayed()))
+        onView(withText(
+            String.format(
+                activityRule.activity.getString(R.string.about_version),
+                version + " " + type + " " + commit)))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun aboutViewOpensGHCommit(){
+        val commit = BuildConfig.COMMIT_SHA1
+        val ghBaseUrl = BuildConfig.GIT_REMOTE
+        val intentResult = ActivityResult(Activity.RESULT_OK, Intent())
+        val ghUrl = ghBaseUrl + "/commit/" + commit
+        intending(hasAction(Intent.ACTION_VIEW)).respondWith(intentResult);
+        onView(withText(
+            String.format(
+                activityRule.activity.getString(R.string.about_android),
+                activityRule.activity.getString(R.string.app_name))))
+            .perform(click())
+        intended(hasData(ghUrl))
+    }
+
 
 }
