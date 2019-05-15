f1fb05c4384b792dc29113741e4bdfc7fffcc397
jrecio <jrecio@solidgear.es>
2019-05-15 12:30:41 +0200

    add test to check send history in log view

--- owncloudApp/src/androidTest/java/com/owncloud/android/settings/more/OCSettingsLogTest.kt
+++ owncloudApp/src/androidTest/java/com/owncloud/android/settings/more/OCSettingsLogTest.kt
@@ -19,22 +19,33 @@
 
 package com.owncloud.android.settings.more
 
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasFlag
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasType
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
+import com.owncloud.android.R
 import com.owncloud.android.ui.activity.LogHistoryActivity
-import com.owncloud.android.ui.activity.Preferences
+import org.hamcrest.Matchers
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
-import com.owncloud.android.R
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import org.junit.runner.RunWith
-import androidx.test.platform.app.InstrumentationRegistry
 
 @RunWith(AndroidJUnit4::class)
 class OCSettingsLogTest {
@@ -52,15 +63,35 @@ class OCSettingsLogTest {
     }
 
     @Test
-    fun checkItemsToolbar(){
+    fun itemsToolbar(){
         onView(withId(R.id.menu_search)).check(matches(isDisplayed()))
         openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().targetContext)
-        onView(withId(R.id.menu_logcat)).check(matches(isDisplayed()))
+        onView(withText("Logcat")).check(matches(isDisplayed()))
     }
 
     @Test
-    fun checkHistoryButtons(){
+    fun logHistoryButtons(){
         onView(withId(R.id.deleteLogHistoryButton)).check(matches(isDisplayed()))
         onView(withId(R.id.sendLogHistoryButton)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun sendLogHistory(){
+        Intents.init()
+        val intentResult = Instrumentation.ActivityResult(Activity.RESULT_OK, Intent())
+        intending(hasAction(Intent.ACTION_SEND_MULTIPLE)).respondWith(intentResult);
+        onView(withId(R.id.sendLogHistoryButton)).perform(click())
+        intended(
+            Matchers.allOf(
+                hasAction(Intent.ACTION_SEND_MULTIPLE),
+                hasExtra(Intent.EXTRA_SUBJECT,
+                    String.format(
+                        activityRule.activity.getString(R.string.log_send_mail_subject),
+                        activityRule.activity.getString(R.string.app_name))),
+                hasType("text/plain"),
+                hasFlag(Intent.FLAG_ACTIVITY_NEW_TASK)
+            )
+        )
+        Intents.release()
     }
 }
