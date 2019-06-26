3562824d6ac0736564a00f042b12fd43ad529e9c
davigonz <dgonzalez@solidgear.es>
2019-06-26 18:11:58 +0200

    Unit tests for RemoteShareesDataSource

---
+++ owncloudApp/src/androidTest/java/com/owncloud/android/sharees/RemoteShareesDataSourceTest.kt
@@ -0,0 +1,18 @@
+package com.owncloud.android.sharees
+
+import com.owncloud.android.lib.common.operations.RemoteOperationResult
+import com.owncloud.android.lib.resources.shares.GetRemoteShareesOperation
+import com.owncloud.android.sharees.data.datasources.RemoteShareesDataSource
+import org.json.JSONObject
+
+class RemoteShareesDataSourceTest(private val remoteOperationResult: RemoteOperationResult<ArrayList<JSONObject>>) :
+    RemoteShareesDataSource {
+    override fun getSharees(
+        searchString: String,
+        page: Int,
+        perPage: Int,
+        getRemoteShareesOperation: GetRemoteShareesOperation
+    ): RemoteOperationResult<ArrayList<JSONObject>> {
+        return remoteOperationResult
+    }
+}
