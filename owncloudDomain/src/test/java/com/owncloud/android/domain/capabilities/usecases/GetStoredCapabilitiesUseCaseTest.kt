d93d841eb51c0836c2b6b72e0eccb07b88d1e97d
davigonz <dgonzalez@solidgear.es>
2019-10-14 13:36:42 +0200

    Include tests for capabilities use cases

---
+++ owncloudDomain/src/test/java/com/owncloud/android/domain/capabilities/usecases/GetStoredCapabilitiesUseCaseTest.kt
@@ -0,0 +1,75 @@
+/**
+ * ownCloud Android client application
+ *
+ * @author David González Verdugo
+ * Copyright (C) 2019 ownCloud GmbH.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.owncloud.android.domain.capabilities.usecases
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.owncloud.android.domain.capabilities.CapabilityRepository
+import com.owncloud.android.domain.capabilities.model.OCCapability
+import com.owncloud.android.domain.utils.DomainTestUtil
+import io.mockk.every
+import io.mockk.spyk
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class GetStoredCapabilitiesUseCaseTest {
+    @Rule
+    @JvmField
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val capabilityRepository: CapabilityRepository = spyk()
+    private val getStoredCapabilitiesUseCase =
+        GetStoredCapabilitiesUseCase((capabilityRepository))
+    private lateinit var capabilitiesEmitted: MutableList<OCCapability>
+
+    @Before
+    fun init() {
+        capabilitiesEmitted = mutableListOf()
+    }
+
+    @Test
+    fun getStoredCapabilitiesOk() {
+        val capabilitiesLiveData = MutableLiveData<OCCapability>()
+        every { capabilityRepository.getStoredCapabilities(any()) } returns capabilitiesLiveData
+
+        val capabilitiesToEmit = listOf(DomainTestUtil.createCapability())
+
+        getStoredCapabilitiesUseCase.execute(
+            GetStoredCapabilitiesUseCase.Params("")
+        ).observeForever {
+            capabilitiesEmitted.add(it!!)
+        }
+
+        capabilitiesToEmit.forEach { capabilitiesLiveData.postValue(it) }
+
+        Assert.assertEquals(capabilitiesToEmit, capabilitiesEmitted)
+    }
+
+    @Test(expected = Exception::class)
+    fun getStoredCapabilitiesException() {
+        every { capabilityRepository.getStoredCapabilities(any()) } throws Exception()
+
+        getStoredCapabilitiesUseCase.execute(
+            GetStoredCapabilitiesUseCase.Params("")
+        )
+    }
+}
---
+++ owncloudDomain/src/test/java/com/owncloud/android/domain/capabilities/usecases/RefreshCapabilitiesFromServerAsyncUseCaseTest.kt
@@ -0,0 +1,66 @@
+/**
+ * ownCloud Android client application
+ *
+ * @author David González Verdugo
+ * Copyright (C) 2019 ownCloud GmbH.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.owncloud.android.domain.capabilities.usecases
+
+import com.owncloud.android.domain.capabilities.CapabilityRepository
+import com.owncloud.android.domain.exceptions.UnauthorizedException
+import io.mockk.every
+import io.mockk.spyk
+import io.mockk.verify
+import org.junit.Assert
+import org.junit.Test
+
+class RefreshCapabilitiesFromServerAsyncUseCaseTest {
+    private val capabilityRepository: CapabilityRepository = spyk()
+    private val refreshCapabilitiesFromServerAsyncUseCase =
+        RefreshCapabilitiesFromServerAsyncUseCase((capabilityRepository))
+
+    @Test
+    fun refreshCapabilitiesFromServerOk() {
+        val useCaseResult =
+            refreshCapabilitiesFromServerAsyncUseCase.execute(RefreshCapabilitiesFromServerAsyncUseCase.Params(""))
+
+        Assert.assertTrue(useCaseResult.isSuccess)
+        Assert.assertFalse(useCaseResult.isError)
+        Assert.assertEquals(Unit, useCaseResult.getDataOrNull())
+
+        verify(exactly = 1) {
+            capabilityRepository.refreshCapabilitiesForAccount("")
+        }
+    }
+
+    @Test
+    fun refreshCapabilitiesFromServerWithUnauthorizedException() {
+        every { capabilityRepository.refreshCapabilitiesForAccount(any()) } throws UnauthorizedException()
+
+        val useCaseResult =
+            refreshCapabilitiesFromServerAsyncUseCase.execute(RefreshCapabilitiesFromServerAsyncUseCase.Params(""))
+
+        Assert.assertFalse(useCaseResult.isSuccess)
+        Assert.assertTrue(useCaseResult.isError)
+
+        Assert.assertNull(useCaseResult.getDataOrNull())
+        Assert.assertTrue(useCaseResult.getThrowableOrNull() is UnauthorizedException)
+
+        verify(exactly = 1) {
+            capabilityRepository.refreshCapabilitiesForAccount("")
+        }
+    }
+}
