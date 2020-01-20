bc900fdcd1e313d7cf6b53de128bddbc9248f340
Abel García de Prada <abelgardep@gmail.com>
2020-01-20 12:54:18 +0100

    Fix crash when trying to connect to self signed certificate

--- owncloudApp/src/main/java/com/owncloud/android/authentication/AuthenticatorActivity.java
+++ owncloudApp/src/main/java/com/owncloud/android/authentication/AuthenticatorActivity.java
@@ -115,7 +115,7 @@ import static android.content.Intent.ACTION_VIEW;
  */
 public class AuthenticatorActivity extends AccountAuthenticatorActivity
         implements OnRemoteOperationListener, OnFocusChangeListener, OnEditorActionListener,
-        AuthenticatorAsyncTask.OnAuthenticatorTaskListener {
+        AuthenticatorAsyncTask.OnAuthenticatorTaskListener, SslUntrustedCertDialog.OnSslUntrustedCertListener {
 
     public static final String EXTRA_ACTION = "ACTION";
     public static final String EXTRA_ACCOUNT = "ACCOUNT";
@@ -1687,6 +1687,23 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
             }
         }
         return false;   // always return false to grant that the software keyboard is hidden anyway
+    }
+
+    @Override
+    public void onSavedCertificate() {
+        Log_OC.d("Server certificate is trusted");
+        checkOcServer();
+    }
+
+    @Override
+    public void onFailedSavingCertificate() {
+        Log_OC.d("Server certificate could not be saved");
+        Toast.makeText(this, R.string.ssl_validator_not_saved, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onCancelCertificate() {
+        Log_OC.d("Server certificate is not trusted");
     }
 
     private abstract static class RightDrawableOnTouchListener implements OnTouchListener {
