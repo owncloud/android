880c628784c08788deef16776a5af1a7c7ee78e4
agarcia <agarcia@solidgeargroup.com>
2020-02-07 11:47:07 +0100

    Rename remote operations

--- owncloudApp/src/main/java/com/owncloud/android/authentication/AuthenticatorAsyncTask.java
+++ owncloudApp/src/main/java/com/owncloud/android/authentication/AuthenticatorAsyncTask.java
@@ -32,7 +32,6 @@ import com.owncloud.android.lib.common.network.RedirectionPath;
 import com.owncloud.android.lib.common.operations.RemoteOperationResult;
 import com.owncloud.android.lib.resources.files.CheckPathExistenceRemoteOperation;
 import com.owncloud.android.lib.resources.users.GetRemoteUserInfoOperation;
-import com.owncloud.android.operations.common.UseCaseHelper;
 
 import java.lang.ref.WeakReference;
 
