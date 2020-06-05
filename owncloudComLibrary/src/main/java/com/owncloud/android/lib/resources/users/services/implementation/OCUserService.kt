59229aa6ab27f46692b5c60b1127d4b7e0248279
agarcia <agarcia@solidgeargroup.com>
2020-06-05 12:32:36 +0200

    Remove avatar dimension from Service constructor

--- owncloudComLibrary/src/main/java/com/owncloud/android/lib/resources/users/services/implementation/OCUserService.kt
+++ owncloudComLibrary/src/main/java/com/owncloud/android/lib/resources/users/services/implementation/OCUserService.kt
@@ -28,14 +28,14 @@ import com.owncloud.android.lib.resources.users.RemoteAvatarData
 import com.owncloud.android.lib.resources.users.RemoteUserInfo
 import com.owncloud.android.lib.resources.users.services.UserService
 
-class OCUserService(override val client: OwnCloudClient, private val avatarDimension: Int) : UserService {
+class OCUserService(override val client: OwnCloudClient) : UserService {
     override fun getUserInfo(): RemoteOperationResult<RemoteUserInfo> =
         GetRemoteUserInfoOperation().execute(client)
 
     override fun getUserQuota(): RemoteOperationResult<GetRemoteUserQuotaOperation.RemoteQuota> =
         GetRemoteUserQuotaOperation().execute(client)
 
-    override fun getUserAvatar(): RemoteOperationResult<RemoteAvatarData> =
+    override fun getUserAvatar(avatarDimension: Int): RemoteOperationResult<RemoteAvatarData> =
         GetRemoteUserAvatarOperation(avatarDimension).execute(client)
 
 }
