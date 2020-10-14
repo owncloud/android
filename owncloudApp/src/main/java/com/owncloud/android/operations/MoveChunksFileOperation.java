173a31dfc6ef619f64f78eaf8a40998a25ac1039
Abel García de Prada <agdeprada@izertis.com>
2020-10-14 12:42:49 +0200

    Deprecate old OCFile and fix sharing tests

--- owncloudApp/src/main/java/com/owncloud/android/operations/MoveChunksFileOperation.java
+++ owncloudApp/src/main/java/com/owncloud/android/operations/MoveChunksFileOperation.java
@@ -19,7 +19,7 @@
 
 package com.owncloud.android.operations;
 
-import com.owncloud.android.datamodel.OCFile;
+import com.owncloud.android.domain.files.model.OCFile;
 import com.owncloud.android.lib.common.OwnCloudClient;
 import com.owncloud.android.lib.common.operations.RemoteOperationResult;
 import com.owncloud.android.lib.resources.files.chunks.MoveRemoteChunksFileOperation;
@@ -34,10 +34,11 @@ public class MoveChunksFileOperation extends MoveFileOperation {
 
     /**
      * Constructor
-     * @param srcPath          Remote path of the {@link OCFile} to move.
-     * @param targetParentPath  Path to the folder where the file will be moved into.
+     *
+     * @param srcPath                Remote path of the {@link OCFile} to move.
+     * @param targetParentPath       Path to the folder where the file will be moved into.
      * @param fileLastModifTimestamp Timestamp of last modification of file to move.
-     * @param fileLength        Total length of the file to move.
+     * @param fileLength             Total length of the file to move.
      */
     public MoveChunksFileOperation(String srcPath, String targetParentPath, String fileLastModifTimestamp,
                                    long fileLength) {
