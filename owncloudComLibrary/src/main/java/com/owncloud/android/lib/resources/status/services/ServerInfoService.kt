0fc3f7668ddce2af276b0538b80616f03c33f92f
Abel García de Prada <agdeprada@izertis.com>
2020-11-19 19:05:48 +0100

    Use the same ownCloudClient across the whole login process

--- owncloudComLibrary/src/main/java/com/owncloud/android/lib/resources/status/services/ServerInfoService.kt
+++ owncloudComLibrary/src/main/java/com/owncloud/android/lib/resources/status/services/ServerInfoService.kt
@@ -23,11 +23,12 @@
  */
 package com.owncloud.android.lib.resources.status.services
 
+import com.owncloud.android.lib.common.OwnCloudClient
 import com.owncloud.android.lib.common.operations.RemoteOperationResult
 import com.owncloud.android.lib.resources.status.OwnCloudVersion
 
 interface ServerInfoService {
-    fun checkPathExistence(path: String, isUserLogged: Boolean): RemoteOperationResult<Boolean>
+    fun checkPathExistence(path: String, isUserLogged: Boolean, client: OwnCloudClient): RemoteOperationResult<Boolean>
 
-    fun getRemoteStatus(path: String): RemoteOperationResult<OwnCloudVersion>
+    fun getRemoteStatus(path: String, client: OwnCloudClient): RemoteOperationResult<OwnCloudVersion>
 }
