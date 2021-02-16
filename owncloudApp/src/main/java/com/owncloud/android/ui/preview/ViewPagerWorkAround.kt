c4730abae7d7306866d43b3f6bb806d3aa8df848
Abel García de Prada <agdeprada@izertis.com>
2021-02-16 10:55:29 +0100

    Fix viewpager crash

---
+++ owncloudApp/src/main/java/com/owncloud/android/ui/preview/ViewPagerWorkAround.kt
@@ -0,0 +1,34 @@
+package com.owncloud.android.ui.preview
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.viewpager.widget.ViewPager
+
+/**
+ * [Workaround](https://github.com/Baseflow/PhotoView/issues/31#issuecomment-19803926)
+ */
+class ViewPagerWorkAround : ViewPager {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {}
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(ev: MotionEvent?): Boolean {
+        try {
+            return super.onTouchEvent(ev)
+        } catch (ex: IllegalArgumentException) {
+            ex.printStackTrace()
+        }
+        return false
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+        try {
+            return super.onInterceptTouchEvent(ev)
+        } catch (ex: IllegalArgumentException) {
+            ex.printStackTrace()
+        }
+        return false
+    }
+}
