c2ee38f63045555c48750c33b6788f48f2d5851a
JuancaG05 <juanca_arrabal@hotmail.com>
2021-02-22 12:24:59 +0100

    Minor changes and navigation up to home in ActionBar enabled

--- owncloudApp/src/main/java/com/owncloud/android/presentation/ui/settings/SettingsActivity.kt
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/ui/settings/SettingsActivity.kt
@@ -20,11 +20,14 @@
 
 package com.owncloud.android.presentation.ui.settings
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.owncloud.android.R
 import com.owncloud.android.presentation.ui.settings.fragments.SettingsFragment
+import com.owncloud.android.ui.activity.FileDisplayActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,5 +47,17 @@ class SettingsActivity : AppCompatActivity() {
                 SettingsFragment()
             )
             .commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                intent = Intent(this, FileDisplayActivity::class.java).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                }
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
