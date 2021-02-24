cbba0e089e452e3f8742b875e56c6a7ec3cc3d2d
JuancaG05 <juanca_arrabal@hotmail.com>
2021-02-24 12:30:27 +0100

    Some more tests, but with a bug in one of them that needs to be solved

--- owncloudApp/src/androidTest/java/com/owncloud/android/settings/security/SettingsFragmentTest.kt
+++ owncloudApp/src/androidTest/java/com/owncloud/android/settings/security/SettingsFragmentTest.kt
@@ -20,19 +20,36 @@
 
 package com.owncloud.android.settings.security
 
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
 import androidx.fragment.app.testing.FragmentScenario
-import androidx.fragment.app.testing.launchFragment
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.preference.CheckBoxPreference
+import androidx.preference.PreferenceManager
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.PreferenceMatchers.*
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isEnabled
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.platform.app.InstrumentationRegistry
 import com.owncloud.android.R
 import com.owncloud.android.presentation.ui.settings.fragments.SettingsFragment
+import com.owncloud.android.ui.activity.PassCodeActivity
+import com.owncloud.android.ui.activity.PatternLockActivity
 import org.hamcrest.Matchers
+import org.junit.After
+import org.junit.Assert
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
@@ -40,9 +57,33 @@ class SettingsFragmentTest {
 
     private lateinit var fragmentScenario: FragmentScenario<SettingsFragment>
 
+    private lateinit var prefPasscode: CheckBoxPreference
+
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+
+    private val keyCheckResult = "KEY_CHECK_RESULT"
+    private val keyPassCode = "KEY_PASSCODE"
+    private val keyPattern = "KEY_PATTERN"
+    private val keyCheckPatternResult = "KEY_CHECK_PATTERN_RESULT"
+
+    private val passCodeValue = "1111"
+    private val patternValue = "1234"
+
+    private val keyPrefBiometric = "set_biometric"
+
     @Before
     fun setUp() {
         fragmentScenario = launchFragmentInContainer()
+        fragmentScenario.onFragment { fragment ->
+            prefPasscode = fragment.findPreference(PatternLockActivity.PREFERENCE_SET_PATTERN)!!
+        }
+        Intents.init()
+    }
+
+    @After
+    fun tearDown() {
+        Intents.release()
+        PreferenceManager.getDefaultSharedPreferences(context).edit().clear().commit()
     }
 
     @Test
@@ -54,5 +95,29 @@ class SettingsFragmentTest {
         onView(withText(R.string.prefs_biometric)).check(matches(Matchers.not(isEnabled())))
         onView(withText(R.string.prefs_touches_with_other_visible_windows)).check(matches(isDisplayed()))
         onView(withText(R.string.prefs_touches_with_other_visible_windows_summary)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun passcodeOpen() {
+        onView(withText(R.string.prefs_passcode)).perform(click())
+        intended(hasComponent(PassCodeActivity::class.java.name))
+    }
+
+    @Test
+    fun patternOpen() {
+        onView(withText(R.string.prefs_pattern)).perform(click())
+        intended(hasComponent(PatternLockActivity::class.java.name))
+    }
+
+    @Test
+    fun passcodeLockEnabled() {
+        val result = Intent()
+        result.putExtra(keyPassCode, passCodeValue)
+        val intentResult = Instrumentation.ActivityResult(Activity.RESULT_OK, result)
+        intending(hasAction(PassCodeActivity.ACTION_REQUEST_WITH_RESULT)).respondWith(intentResult)
+        onView(withText(R.string.prefs_passcode)).perform(click())
+        //onData(withKey(keyPrefBiometric)).check(matches(isEnabled()))
+        assertTrue(prefPasscode.isChecked)
+
     }
 }
