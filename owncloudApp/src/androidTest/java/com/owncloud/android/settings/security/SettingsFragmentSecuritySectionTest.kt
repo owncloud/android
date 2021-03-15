19e4324c2ece1b4971fc5094820ff775ba8823b9
JuancaG05 <juanca_arrabal@hotmail.com>
2021-03-15 11:07:44 +0100

    Hidden logs category in the other UI tests

--- owncloudApp/src/androidTest/java/com/owncloud/android/settings/security/SettingsFragmentSecuritySectionTest.kt
+++ owncloudApp/src/androidTest/java/com/owncloud/android/settings/security/SettingsFragmentSecuritySectionTest.kt
@@ -64,6 +64,7 @@ class SettingsFragmentSecuritySectionTest {
 
     private lateinit var fragmentScenario: FragmentScenario<SettingsFragment>
 
+    private lateinit var prefLogsCategory: PreferenceCategory
     private lateinit var prefMoreCategory: PreferenceCategory
     private lateinit var prefSecurityCategory: PreferenceCategory
     private lateinit var prefPasscode: CheckBoxPreference
@@ -105,6 +106,7 @@ class SettingsFragmentSecuritySectionTest {
 
         fragmentScenario = launchFragmentInContainer(themeResId = R.style.Theme_ownCloud)
         fragmentScenario.onFragment { fragment ->
+            prefLogsCategory = fragment.findPreference(PREFERENCE_LOGS_CATEGORY)!!
             prefMoreCategory = fragment.findPreference(PREFERENCE_MORE_CATEGORY)!!
             prefSecurityCategory = fragment.findPreference(PREFERENCE_SECURITY_CATEGORY)!!
             prefPasscode = fragment.findPreference(PassCodeActivity.PREFERENCE_SET_PASSCODE)!!
@@ -114,6 +116,7 @@ class SettingsFragmentSecuritySectionTest {
                 fragment.findPreference(SettingsFragment.PREFERENCE_TOUCHES_WITH_OTHER_VISIBLE_WINDOWS)!!
         }
 
+        prefLogsCategory.isVisible = false
         prefMoreCategory.isVisible = false
 
         Intents.init()
@@ -448,6 +451,7 @@ class SettingsFragmentSecuritySectionTest {
 
     companion object {
         private const val PREFERENCE_SECURITY_CATEGORY = "security_category"
+        private const val PREFERENCE_LOGS_CATEGORY = "logs_category"
         private const val PREFERENCE_MORE_CATEGORY = "more_category"
     }
 }
