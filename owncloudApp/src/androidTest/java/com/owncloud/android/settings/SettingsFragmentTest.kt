04049e15ec5679423629476d8c82cec1abea4416
JuancaG05 <juanca_arrabal@hotmail.com>
2021-03-25 13:09:04 +0100

    Removed some unused imports

--- owncloudApp/src/androidTest/java/com/owncloud/android/settings/SettingsFragmentTest.kt
+++ owncloudApp/src/androidTest/java/com/owncloud/android/settings/SettingsFragmentTest.kt
@@ -27,9 +27,7 @@ import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.preference.Preference
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isEnabled
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
