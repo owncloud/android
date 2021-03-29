e1679d3391ea95e61b64781ac159faa9de762870
Abel García de Prada <agdeprada@izertis.com>
2021-03-29 13:02:19 +0200

    Image preview is working now. Refactor needed.

--- owncloudApp/src/main/java/com/owncloud/android/dependecyinjection/CommonModule.kt
+++ owncloudApp/src/main/java/com/owncloud/android/dependecyinjection/CommonModule.kt
@@ -22,11 +22,13 @@ package com.owncloud.android.dependecyinjection
 
 import com.owncloud.android.presentation.manager.AvatarManager
 import com.owncloud.android.providers.AccountProvider
+import com.owncloud.android.presentation.manager.TransferManager
 import com.owncloud.android.providers.CameraUploadsHandlerProvider
 import com.owncloud.android.providers.ContextProvider
 import com.owncloud.android.providers.CoroutinesDispatcherProvider
 import com.owncloud.android.providers.LogsProvider
 import com.owncloud.android.providers.impl.OCContextProvider
+import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
@@ -38,4 +40,5 @@ val commonModule = module {
     single { LogsProvider(get()) }
     single { CameraUploadsHandlerProvider(androidContext()) }
     single { AccountProvider(androidContext()) }
+    single { TransferManager(androidApplication()) }
 }
