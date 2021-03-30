a378cacf9caebbdc8b2050caa644d6edad6fb82a
Abel García de Prada <agdeprada@izertis.com>
2021-03-30 12:20:19 +0200

    Refactor FileDetailFragment. Download progress is shown. Download finished WIP

--- owncloudApp/src/main/java/com/owncloud/android/dependecyinjection/ViewModelModule.kt
+++ owncloudApp/src/main/java/com/owncloud/android/dependecyinjection/ViewModelModule.kt
@@ -27,6 +27,7 @@ import com.owncloud.android.presentation.ui.security.passcode.PasscodeAction
 import com.owncloud.android.presentation.viewmodels.authentication.OCAuthenticationViewModel
 import com.owncloud.android.presentation.viewmodels.capabilities.OCCapabilityViewModel
 import com.owncloud.android.presentation.viewmodels.drawer.DrawerViewModel
+import com.owncloud.android.presentation.viewmodels.files.FileDetailsViewModel
 import com.owncloud.android.presentation.viewmodels.files.FilesViewModel
 import com.owncloud.android.presentation.viewmodels.logging.LogListViewModel
 import com.owncloud.android.presentation.viewmodels.migration.MigrationViewModel
@@ -82,4 +83,5 @@ val viewModelModule = module {
     viewModel { FilesViewModel(get(), get(), get()) }
 
     viewModel { PreviewImageViewModel(get(), get(), get()) }
+    viewModel { FileDetailsViewModel(get()) }
 }
