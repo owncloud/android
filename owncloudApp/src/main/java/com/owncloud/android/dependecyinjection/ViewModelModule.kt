991f031bf184de0bf412f31f626af476711c0585
Abel García de Prada <agdeprada@izertis.com>
2021-03-31 12:10:17 +0200

    Show preview after download in file details view.

--- owncloudApp/src/main/java/com/owncloud/android/dependecyinjection/ViewModelModule.kt
+++ owncloudApp/src/main/java/com/owncloud/android/dependecyinjection/ViewModelModule.kt
@@ -64,5 +64,5 @@ val viewModelModule = module {
     viewModel { FilesViewModel(get(), get(), get()) }
 
     viewModel { PreviewImageViewModel(get(), get(), get()) }
-    viewModel { FileDetailsViewModel(get()) }
+    viewModel { FileDetailsViewModel(get(), get(), get()) }
 }
