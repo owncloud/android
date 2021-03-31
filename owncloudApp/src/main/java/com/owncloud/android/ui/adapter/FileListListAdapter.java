3040c926d75572cbecc8acefbd887071b1d5e5d6
Abel García de Prada <agdeprada@izertis.com>
2021-03-31 14:45:39 +0200

    Remove old FileDownloader

--- owncloudApp/src/main/java/com/owncloud/android/ui/adapter/FileListListAdapter.java
+++ owncloudApp/src/main/java/com/owncloud/android/ui/adapter/FileListListAdapter.java
@@ -53,9 +53,9 @@ import com.owncloud.android.datamodel.FileDataStorageManager;
 import com.owncloud.android.datamodel.ThumbnailsCacheManager;
 import com.owncloud.android.db.PreferenceManager;
 import com.owncloud.android.domain.files.model.OCFile;
-import com.owncloud.android.files.services.FileDownloader.FileDownloaderBinder;
 import com.owncloud.android.files.services.FileUploader.FileUploaderBinder;
 import com.owncloud.android.presentation.ui.settings.fragments.SettingsAdvancedFragment;
+import com.owncloud.android.presentation.manager.TransferManager;
 import com.owncloud.android.services.OperationsService.OperationsServiceBinder;
 import com.owncloud.android.ui.activity.ComponentsGetter;
 import com.owncloud.android.utils.DisplayUtils;
@@ -328,8 +328,7 @@ public class FileListListAdapter extends BaseAdapter implements ListAdapter {
     private void setIconPinAccordingToFilesLocalState(ImageView localStateView, OCFile file) {
         // local state
         localStateView.bringToFront();
-        final FileDownloaderBinder downloaderBinder =
-                mTransferServiceGetter.getFileDownloaderBinder();
+        final TransferManager transferManager = new TransferManager(mContext);
         final FileUploaderBinder uploaderBinder =
                 mTransferServiceGetter.getFileUploaderBinder();
         final OperationsServiceBinder opsBinder =
@@ -341,7 +340,7 @@ public class FileListListAdapter extends BaseAdapter implements ListAdapter {
             //syncing
             localStateView.setImageResource(R.drawable.sync_pin);
             localStateView.setVisibility(View.VISIBLE);
-        } else if (downloaderBinder != null && downloaderBinder.isDownloading(mAccount, file)) {
+        } else if (transferManager.isDownloadPending(mAccount, file)) {
             // downloading
             localStateView.setImageResource(R.drawable.sync_pin);
             localStateView.setVisibility(View.VISIBLE);
