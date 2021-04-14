9acfd63cf6b393e70a1e59ed1a2e9ed597a546b1
Abel García de Prada <agdeprada@izertis.com>
2021-04-14 16:01:56 +0200

    Make some fields not mandatory in discovery response

--- owncloudDomain/src/main/java/com/owncloud/android/domain/authentication/oauth/model/OIDCServerConfiguration.kt
+++ owncloudDomain/src/main/java/com/owncloud/android/domain/authentication/oauth/model/OIDCServerConfiguration.kt
@@ -20,13 +20,13 @@ package com.owncloud.android.domain.authentication.oauth.model
 
 data class OIDCServerConfiguration(
     val authorizationEndpoint: String,
-    val checkSessionIframe: String,
-    val endSessionEndpoint: String,
+    val checkSessionIframe: String?,
+    val endSessionEndpoint: String?,
     val issuer: String,
-    val registrationEndpoint: String,
+    val registrationEndpoint: String?,
     val responseTypesSupported: List<String>,
-    val scopesSupported: List<String>,
+    val scopesSupported: List<String>?,
     val tokenEndpoint: String,
-    val tokenEndpointAuthMethodsSupported: List<String>,
-    val userInfoEndpoint: String,
+    val tokenEndpointAuthMethodsSupported: List<String>?,
+    val userInfoEndpoint: String?,
 )
