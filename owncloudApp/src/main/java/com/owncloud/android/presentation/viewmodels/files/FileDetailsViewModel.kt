15a2c3d68abcd846f760062cb5476bd75d22de8c
Abel García de Prada <agdeprada@izertis.com>
2021-04-27 17:29:25 +0200

    Apply code review suggestions

--- owncloudApp/src/main/java/com/owncloud/android/presentation/viewmodels/files/FileDetailsViewModel.kt
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/viewmodels/files/FileDetailsViewModel.kt
@@ -21,6 +21,7 @@ package com.owncloud.android.presentation.viewmodels.files
 import android.accounts.Account
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.work.WorkInfo
