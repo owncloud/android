7f5946388b4361cf1303e81ba4e186215fdf52f6
Abel García de Prada <agdeprada@izertis.com>
2021-04-29 11:18:06 +0200

    Add remove file to RemoteDataSource

--- owncloudApp/src/main/java/com/owncloud/android/dependecyinjection/RemoteDataSourceModule.kt
+++ owncloudApp/src/main/java/com/owncloud/android/dependecyinjection/RemoteDataSourceModule.kt
@@ -46,7 +46,9 @@ import com.owncloud.android.data.user.datasources.implementation.OCRemoteUserDat
 import com.owncloud.android.lib.common.ConnectionValidator
 import com.owncloud.android.lib.common.OwnCloudAccount
 import com.owncloud.android.lib.common.SingleSessionManager
+import com.owncloud.android.lib.resources.files.services.ChunkService
 import com.owncloud.android.lib.resources.files.services.FileService
+import com.owncloud.android.lib.resources.files.services.implementation.OCChunkService
 import com.owncloud.android.lib.resources.files.services.implementation.OCFileService
 import com.owncloud.android.lib.resources.oauth.services.OIDCService
 import com.owncloud.android.lib.resources.oauth.services.implementation.OCOIDCService
@@ -71,6 +73,7 @@ val remoteDataSourceModule = module {
 
     single<CapabilityService> { OCCapabilityService(get()) }
     single<FileService> { OCFileService(get()) }
+    single<ChunkService> { OCChunkService(get()) }
     single<ServerInfoService> { OCServerInfoService() }
     single<OIDCService> { OCOIDCService() }
     single<ShareService> { OCShareService(get()) }
