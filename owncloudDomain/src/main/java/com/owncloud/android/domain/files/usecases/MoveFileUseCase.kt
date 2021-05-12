19cbc9e45ee52a9e2a1d147213085840830cba44
Abel García de Prada <agdeprada@izertis.com>
2021-05-12 08:34:46 +0200

    Add move file use case

---
+++ owncloudDomain/src/main/java/com/owncloud/android/domain/files/usecases/MoveFileUseCase.kt
@@ -0,0 +1,43 @@
+/**
+ * ownCloud Android client application
+ *
+ * @author Abel García de Prada
+ * Copyright (C) 2021 ownCloud GmbH.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.owncloud.android.domain.files.usecases
+
+import com.owncloud.android.domain.BaseUseCaseWithResult
+import com.owncloud.android.domain.files.FileRepository
+import com.owncloud.android.domain.files.model.OCFile
+
+class MoveFileUseCase(
+    private val fileRepository: FileRepository
+) : BaseUseCaseWithResult<Unit, MoveFileUseCase.Params>() {
+
+    override fun run(params: Params) {
+
+        require(params.listOfFilesToMove.isNotEmpty())
+
+        return fileRepository.moveFile(
+            listOfFilesToMove = params.listOfFilesToMove,
+            targetFile = params.targetFile
+        )
+    }
+
+    data class Params(
+        val listOfFilesToMove: List<OCFile>,
+        val targetFile: OCFile
+    )
+}
