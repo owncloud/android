6373f4c17f4fbff7d9472095a6c8001d4d2243a8
Abel García de Prada <agdeprada@izertis.com>
2021-05-12 08:47:23 +0200

    Implement move file in remote data source

--- owncloudData/src/main/java/com/owncloud/android/data/files/datasources/RemoteFileDataSource.kt
+++ owncloudData/src/main/java/com/owncloud/android/data/files/datasources/RemoteFileDataSource.kt
@@ -27,6 +27,8 @@ interface RemoteFileDataSource {
 
     fun createFolder(remotePath: String, createFullPath: Boolean, isChunksFolder: Boolean)
 
+    fun moveFile(sourceRemotePath: String, targetRemotePath: String)
+
     fun refreshFolder(remotePath: String): List<OCFile>
 
     fun removeFile(remotePath: String)
