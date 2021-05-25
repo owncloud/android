744482dba6a35f53092fb50e45898fec2b0d0146
Abel García de Prada <agdeprada@izertis.com>
2021-05-25 16:07:22 +0200

    Simplify copy file in database

--- owncloudData/src/main/java/com/owncloud/android/data/files/repository/OCFileRepository.kt
+++ owncloudData/src/main/java/com/owncloud/android/data/files/repository/OCFileRepository.kt
@@ -70,10 +70,8 @@ class OCFileRepository(
 
             // 1. Get the final remote path for this file.
             val expectedRemotePath: String = targetFolder.remotePath + ocFile.fileName
-            val finalRemotePath: String = remoteFileDataSource.getAvailableRemotePath(expectedRemotePath).apply {
-                if (ocFile.isFolder) {
-                    plus(File.separator)
-                }
+            val finalRemotePath: String = remoteFileDataSource.getAvailableRemotePath(expectedRemotePath).let {
+                if (ocFile.isFolder) it.plus(File.separator) else it
             }
 
             // 2. Try to copy files in server
@@ -126,10 +124,8 @@ class OCFileRepository(
 
             // 1. Get the final remote path for this file.
             val expectedRemotePath: String = targetFile.remotePath + ocFile.fileName
-            val finalRemotePath: String = remoteFileDataSource.getAvailableRemotePath(expectedRemotePath).apply {
-                if (ocFile.isFolder) {
-                    plus(File.separator)
-                }
+            val finalRemotePath: String = remoteFileDataSource.getAvailableRemotePath(expectedRemotePath).let {
+                if (ocFile.isFolder) it.plus(File.separator) else it
             }
             val finalStoragePath: String = localStorageProvider.getDefaultSavePathFor(targetFile.owner, finalRemotePath)
 
