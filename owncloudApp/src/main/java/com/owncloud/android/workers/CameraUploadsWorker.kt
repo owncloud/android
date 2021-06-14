643eea672c21a11c4c9d846da8080d62271b4912
Abel García de Prada <agdeprada@izertis.com>
2021-06-14 12:47:18 +0200

    Sample to upload files from content Uri - WIP: Move it to a different worker

--- owncloudApp/src/main/java/com/owncloud/android/workers/CameraUploadsWorker.kt
+++ owncloudApp/src/main/java/com/owncloud/android/workers/CameraUploadsWorker.kt
@@ -1,20 +1,34 @@
 package com.owncloud.android.workers
 
+import android.content.ContentResolver
 import android.content.Context
 import android.net.Uri
 import androidx.core.net.toUri
 import androidx.documentfile.provider.DocumentFile
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.owncloud.android.authentication.AccountUtils
 import com.owncloud.android.datamodel.CameraUploadsSyncStorageManager
 import com.owncloud.android.domain.UseCaseResult
 import com.owncloud.android.domain.camerauploads.model.FolderBackUpConfiguration
 import com.owncloud.android.domain.camerauploads.usecases.GetCameraUploadsConfigurationUseCase
+import com.owncloud.android.lib.common.OwnCloudAccount
+import com.owncloud.android.lib.common.SingleSessionManager
+import com.owncloud.android.lib.common.http.HttpConstants
+import com.owncloud.android.lib.common.http.methods.webdav.PutMethod
+import com.owncloud.android.lib.common.network.WebdavUtils
 import com.owncloud.android.utils.MimetypeIconUtil
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import okio.BufferedSink
+import okio.source
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import timber.log.Timber
 import java.io.File
+import java.io.IOException
+import java.net.URL
 import java.util.concurrent.TimeUnit
 
 class CameraUploadsWorker(
@@ -67,8 +81,13 @@ class CameraUploadsWorker(
                     if (documentFile.lastModified() <= cameraUploadSync.picturesLastSync) {
                         Timber.i("Image ${documentFile.name} created before period to check, ignoring... Last Modified: ${documentFile.lastModified()} Last sync: ${cameraUploadSync.picturesLastSync}")
                     } else {
-                        // TODO: Upload document file
                         Timber.d("Upload document file ${documentFile.name}")
+                        uploadDocument(
+                            documentFile.uri,
+                            appContext,
+                            pictureUploadsConfiguration.uploadPath.plus(documentFile.name),
+                            (documentFile.lastModified()/1000L).toString()
+                        )
                     }
                 }
             }
@@ -97,6 +116,42 @@ class CameraUploadsWorker(
                 }
             }
         }
+    }
+
+    class ContentUriRequestBody(
+        private val contentResolver: ContentResolver,
+        private val contentUri: Uri
+    ) : RequestBody() {
+
+        override fun contentType(): MediaType? {
+            val contentType = contentResolver.getType(contentUri) ?: return null
+            return contentType.toMediaTypeOrNull()
+        }
+
+        override fun writeTo(sink: BufferedSink) {
+            val inputStream = contentResolver.openInputStream(contentUri)
+                ?: throw IOException("Couldn't open content URI for reading: $contentUri")
+
+            inputStream.source().use { source ->
+                sink.writeAll(source)
+            }
+        }
+    }
+
+    fun uploadDocument(contentUri: Uri, context: Context, remotePath: String, lastModified: String) {
+        val requestBody = ContentUriRequestBody(context.contentResolver, contentUri)
+
+        val client = SingleSessionManager.getDefaultSingleton()
+            .getClientFor(OwnCloudAccount(AccountUtils.getCurrentOwnCloudAccount(context), context), context)
+
+        val putMethod = PutMethod(URL(client.userFilesWebDavUri.toString() + WebdavUtils.encodePath(remotePath)), requestBody)
+
+        putMethod.setRetryOnConnectionFailure(false)
+
+        putMethod.addRequestHeader(HttpConstants.OC_TOTAL_LENGTH_HEADER, requestBody.contentLength().toString())
+        putMethod.addRequestHeader(HttpConstants.OC_X_OC_MTIME_HEADER, lastModified)
+
+        val status: Int = client.executeHttpMethod(putMethod)
     }
 
     companion object {
