6b1debb3f966f2af1126499dbabf948a15daa81a
Josh Soref <jsoref@users.noreply.github.com>
2021-09-15 08:38:07 -0400

    spelling: multiple
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- owncloudApp/src/main/java/com/owncloud/android/ui/activity/UploadListActivity.java
+++ owncloudApp/src/main/java/com/owncloud/android/ui/activity/UploadListActivity.java
@@ -76,7 +76,7 @@ public class UploadListActivity extends FileActivity implements UploadListFragme
         View rightFragmentContainer = findViewById(R.id.right_fragment_container);
         rightFragmentContainer.setVisibility(View.GONE);
 
-        // this activity has no file really bound, it's for mulitple accounts at the same time; should no inherit
+        // this activity has no file really bound, it's for multiple accounts at the same time; should no inherit
         // from FileActivity; moreover, some behaviours inherited from FileActivity should be delegated to Fragments;
         // but that's other story
         setFile(null);
