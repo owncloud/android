4abda5f56e67f8ef38d7e0dc4909395ee29bde1c
Josh Soref <jsoref@users.noreply.github.com>
2021-09-20 10:37:08 -0400

    spelling: response
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- owncloudData/src/test/java/com/owncloud/android/data/oauth/datasource/RemoteOAuthDataSourceTest.kt
+++ owncloudData/src/test/java/com/owncloud/android/data/oauth/datasource/RemoteOAuthDataSourceTest.kt
@@ -97,10 +97,10 @@ class RemoteOAuthDataSourceTest {
             oidcService.performTokenRequest(ocClientMocked, any())
         } returns tokenResponseResult
 
-        val tokenResponsed = remoteOAuthDataSource.performTokenRequest(OC_TOKEN_REQUEST_ACCESS)
+        val tokenResponse = remoteOAuthDataSource.performTokenRequest(OC_TOKEN_REQUEST_ACCESS)
 
-        assertNotNull(tokenResponsed)
-        assertEquals(OC_TOKEN_RESPONSE, tokenResponsed)
+        assertNotNull(tokenResponse)
+        assertEquals(OC_TOKEN_RESPONSE, tokenResponse)
     }
 
     @Test(expected = Exception::class)
