9acf51c7577b3605fbd75eb40f1f8455a8c8b54d
Fernando Sanz <fsanz@izertis.com>
2021-11-10 17:28:08 +0100

    Migration value's bug fixed.

--- owncloudData/src/main/java/com/owncloud/android/data/migrations/Migration_36.kt
+++ owncloudData/src/main/java/com/owncloud/android/data/migrations/Migration_36.kt
@@ -28,7 +28,7 @@ import com.owncloud.android.data.ProviderMeta.ProviderTableMeta.FOLDER_BACKUP_TA
 val MIGRATION_35_36 = object : Migration(35, 36) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.run {
-            execSQL("ALTER TABLE $CAPABILITIES_TABLE_NAME ADD COLUMN sharing_user_profile_picture INTEGER NOT NULL DEFAULT '-1'")
+            execSQL("ALTER TABLE $CAPABILITIES_TABLE_NAME ADD COLUMN sharing_user_profile_picture INTEGER NOT NULL DEFAULT -1")
         }
     }
 }
