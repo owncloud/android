e67797a085e82174e373eb25bda38a1964f4b7e9
Fernando Sanz <fsanz@izertis.com>
2021-11-10 16:45:11 +0100

    Bump database's version to 36.

--- owncloudData/src/main/java/com/owncloud/android/data/ProviderMeta.java
+++ owncloudData/src/main/java/com/owncloud/android/data/ProviderMeta.java
@@ -31,7 +31,7 @@ public class ProviderMeta {
 
     public static final String DB_NAME = "filelist";
     public static final String NEW_DB_NAME = "owncloud_database";
-    public static final int DB_VERSION = 35;
+    public static final int DB_VERSION = 36;
 
     private ProviderMeta() {
     }
