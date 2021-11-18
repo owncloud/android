1692d2ff2dc2986615e4d599ef88a96b81523d65
Fernando Sanz <fsanz@izertis.com>
2021-11-18 14:15:02 +0100

    Fragment and its viewmodel has been created.

---
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/ui/files/filelist/MainFileListViewModel.kt
@@ -0,0 +1,7 @@
+package com.owncloud.android.presentation.ui.files.filelist
+
+import androidx.lifecycle.ViewModel
+
+class MainFileListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
