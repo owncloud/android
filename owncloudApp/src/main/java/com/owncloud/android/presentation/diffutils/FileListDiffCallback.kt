6f5c07b36d30b2f45902f9efdfe0bdaae1fd822b
Fernando Sanz <fsanz@izertis.com>
2021-11-19 13:27:16 +0100

    Fragment and its ViewModel have been created, also an Adapter and its DiffUtil have been created too.

---
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/diffutils/FileListDiffCallback.kt
@@ -0,0 +1,41 @@
+/**
+ * ownCloud Android client application
+ *
+ * @author Fernando Sanz Velasco
+ * Copyright (C) 2021 ownCloud GmbH.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.owncloud.android.presentation.diffutils
+
+import androidx.recyclerview.widget.DiffUtil
+import com.owncloud.android.domain.files.model.OCFile
+
+class FileListDiffCallback(private val oldList: List<OCFile>, private val newList: List<OCFile>) : DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition].id === newList[newItemPosition].id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val (_, value, name) = oldList[oldItemPosition]
+        val (_, value1, name1) = newList[newItemPosition]
+        return name == name1 && value == value1
+    }
+}
