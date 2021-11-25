8df20131da49f771e24276ae31f5fb43f4b7c4e2
Jose Antonio Barros <jabarros@solidgear.es>
2021-11-25 11:45:25 +0100

    Changes after CR

--- owncloudApp/src/main/java/com/owncloud/android/presentation/observers/EmptyDataObserver.kt
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/observers/EmptyDataObserver.kt
@@ -1,16 +1,17 @@
 package com.owncloud.android.presentation.observers
 
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 
-class EmptyDataObserver constructor(rv: RecyclerView?, ev: View?): RecyclerView.AdapterDataObserver() {
+class EmptyDataObserver constructor(recyclerView: RecyclerView?, emptyView: View?): RecyclerView.AdapterDataObserver() {
 
     private var emptyView: View? = null
     private var recyclerView: RecyclerView? = null
 
     init {
-        recyclerView = rv
-        emptyView = ev
+        this.recyclerView = recyclerView
+        this.emptyView = emptyView
         checkIfEmpty()
     }
 
@@ -18,8 +19,8 @@ class EmptyDataObserver constructor(rv: RecyclerView?, ev: View?): RecyclerView.
     private fun checkIfEmpty() {
         if (emptyView != null && recyclerView!!.adapter != null) {
             val emptyViewVisible = recyclerView!!.adapter!!.itemCount == 0
-            emptyView!!.visibility = if (emptyViewVisible) View.VISIBLE else View.GONE
-            recyclerView!!.visibility = if (emptyViewVisible) View.GONE else View.VISIBLE
+            emptyView!!.isVisible = emptyViewVisible
+            recyclerView!!.isVisible = !emptyViewVisible
         }
     }
 
