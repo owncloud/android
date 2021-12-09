6c57e023c56cd96cc511c3a69d7cda8e6dca39a0
Fernando Sanz <fsanz@izertis.com>
2021-12-09 10:59:20 +0100

    Footer with Swipe to refresh implemented.

--- owncloudApp/src/main/java/com/owncloud/android/presentation/ui/files/filelist/MainFileListViewModel.kt
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/ui/files/filelist/MainFileListViewModel.kt
@@ -48,14 +48,6 @@ class MainFileListViewModel(
     val getFilesListStatusLiveData: LiveData<Event<UIResult<List<OCFile>>>>
         get() = _getFilesListStatusLiveData
 
-    private val _numberOfFilesPerType = MutableLiveData<Event<UIResult<Pair<Int, Int>>>>()
-    val numberOfFilesPerType: LiveData<Event<UIResult<Pair<Int, Int>>>>
-        get() = _numberOfFilesPerType
-
-    private val _footerText = MutableLiveData<Event<UIResult<String>>>()
-    val footerText: LiveData<Event<UIResult<String>>>
-        get() = _footerText
-
     private fun getFilesList(folderId: Long) {
         val filesListLiveData: LiveData<List<OCFile>> =
             getFolderContentAsLiveDataUseCase.execute(GetFolderContentAsLiveDataUseCase.Params(folderId = folderId))
@@ -70,77 +62,6 @@ class MainFileListViewModel(
             _getFilesListStatusLiveData.postValue(Event(UIResult.Loading()))
             refreshFolderFromServerAsyncUseCase.execute(RefreshFolderFromServerAsyncUseCase.Params(remotePath = remotePath))
         }
-    }
-
-    fun manageListOfFiles(list: List<OCFile>) {
-        var filesCount = 0
-        var foldersCount = 0
-        val count: Int = list.size
-        var file: OCFile
-        for (i in 0 until count) {
-            file = list[i]
-            if (file.isFolder) {
-                foldersCount++
-            } else {
-                if (!file.isHidden) {
-                    filesCount++
-                }
-            }
-        }
-
-        _numberOfFilesPerType.postValue(Event(UIResult.Success(Pair(foldersCount, filesCount))))
-    }
-
-    fun generateFooterText(filesCount: Int, foldersCount: Int) {
-        _footerText.postValue(
-            Event(
-                UIResult.Success(
-                    when {
-                        filesCount <= 0 -> {
-                            when {
-                                foldersCount <= 0 -> {
-                                    ""
-                                }
-                                foldersCount == 1 -> {
-                                    contextProvider.getContext().getString(R.string.file_list__footer__folder)
-                                }
-                                else -> { // foldersCount > 1
-                                    contextProvider.getContext().getString(R.string.file_list__footer__folders, foldersCount)
-                                }
-                            }
-                        }
-                        filesCount == 1 -> {
-                            when {
-                                foldersCount <= 0 -> {
-                                    contextProvider.getContext().getString(R.string.file_list__footer__file)
-                                }
-                                foldersCount == 1 -> {
-                                    contextProvider.getContext().getString(R.string.file_list__footer__file_and_folder)
-                                }
-                                else -> { // foldersCount > 1
-                                    contextProvider.getContext().getString(R.string.file_list__footer__file_and_folders, foldersCount)
-                                }
-                            }
-                        }
-                        else -> {    // filesCount > 1
-                            when {
-                                foldersCount <= 0 -> {
-                                    contextProvider.getContext().getString(R.string.file_list__footer__files, filesCount)
-                                }
-                                foldersCount == 1 -> {
-                                    contextProvider.getContext().getString(R.string.file_list__footer__files_and_folder, filesCount)
-                                }
-                                else -> { // foldersCount > 1
-                                    contextProvider.getContext().getString(
-                                        R.string.file_list__footer__files_and_folders, filesCount, foldersCount
-                                    )
-                                }
-                            }
-                        }
-                    }
-                )
-            )
-        )
     }
 
     fun listDirectory(directory: OCFile) {
