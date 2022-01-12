ceeb93055dfbb1e4bb4fd8d2af522a3ca5f9acc1
Jose Antonio Barros <jabarros@solidgear.es>
2022-01-12 09:13:50 +0100

    Split searches betwenn all files, available offline and shared by link

--- owncloudDomain/src/main/java/com/owncloud/android/domain/files/model/FileListOption.kt
+++ owncloudDomain/src/main/java/com/owncloud/android/domain/files/model/FileListOption.kt
@@ -20,7 +20,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http:></http:>//www.gnu.org/licenses/>.
  */
-package com.owncloud.android.ui.activity
+package com.owncloud.android.domain.files.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
