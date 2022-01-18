2df8b81c53405ec2111441722beb999e2409425f
Abel García de Prada <agdeprada@izertis.com>
2022-01-18 16:01:35 +0100

    First attempt to upload files from file system

--- owncloudApp/src/main/java/com/owncloud/android/dependecyinjection/UseCaseModule.kt
+++ owncloudApp/src/main/java/com/owncloud/android/dependecyinjection/UseCaseModule.kt
@@ -68,6 +68,7 @@ import com.owncloud.android.domain.user.usecases.GetUserAvatarAsyncUseCase
 import com.owncloud.android.domain.user.usecases.GetUserInfoAsyncUseCase
 import com.owncloud.android.domain.user.usecases.RefreshUserQuotaFromServerAsyncUseCase
 import com.owncloud.android.usecases.UploadFileFromSAFUseCase
+import com.owncloud.android.usecases.UploadFileFromSystemUseCase
 import com.owncloud.android.usecases.transfers.CancelDownloadForFileUseCase
 import com.owncloud.android.usecases.transfers.CancelDownloadsForAccountUseCase
 import com.owncloud.android.usecases.transfers.DownloadFileUseCase
@@ -128,6 +129,7 @@ val useCaseModule = module {
     factory { GetLiveDataForDownloadingFileUseCase(get()) }
     factory { GetLiveDataForFinishedDownloadsFromAccountUseCase(get()) }
     factory { UploadFileFromSAFUseCase(get()) }
+    factory { UploadFileFromSystemUseCase(get()) }
 
     // User
     factory { GetStoredQuotaUseCase(get()) }
