d40c2d2cf6d1dda610b51ea5d662064e3f7b036c
Christian Schabesberger <chris.schabesberger@mailbox.org>
2022-03-02 16:53:15 +0100

    check owncloudApp folder for license header

---
+++ check_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+check_directory() {
+    if ! head -n 20 $FILE | grep -q "This program is free software: you can redistribute it and/or modify"
+    then
+        if ! head -n 20 $FILE | grep -q "Licensed under the Apache License, Version 2.0"
+        then
+            echo "$FILE does not contain a current copyright header"
+        fi
+    fi
+}
+
+
+for DIRS in owncloudApp/src owncloudData/src owncloudDomain/src owncloudTestUtil/src
+do
+    for FILE in $(find $DIRS -name "*.java" -o -name "*.kt")
+    do
+        check_directory
+    done
+done
+
+./gradlew ktlintFormat
+
+echo "========= Check Library =========="
+cd owncloud-android-library
+./check_script.sh
