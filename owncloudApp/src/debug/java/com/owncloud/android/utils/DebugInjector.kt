1fdefb904757c738b2badb8e2a44d3cdaae06f62
Fernando Sanz <fsanz@izertis.com>
2022-03-10 11:18:16 +0100

    StrictMode added.

--- owncloudApp/src/debug/java/com/owncloud/android/utils/DebugInjector.kt
+++ owncloudApp/src/debug/java/com/owncloud/android/utils/DebugInjector.kt
@@ -20,10 +20,22 @@
 package com.owncloud.android.utils
 
 import android.content.Context
+import android.os.Build
+import android.os.StrictMode
 import com.facebook.stetho.Stetho
 
 object DebugInjector {
     open fun injectDebugTools(context: Context) {
         Stetho.initializeWithDefaults(context)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .penaltyLog()
+                    .detectUnsafeIntentLaunch()
+                    .detectNonSdkApiUsage()
+                    .build()
+            )
+        }
     }
 }
