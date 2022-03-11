e1534d44b143c73ab7de3bf0087dfb1cce5557b9
davcres <david.crespo.rios@alumnos.uva.es>
2022-03-11 13:03:35 +0100

    Copyright and other requested changes

--- owncloudApp/src/main/java/com/owncloud/android/presentation/ui/security/passcode/NumberKeyboardListener.kt
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/ui/security/passcode/NumberKeyboardListener.kt
@@ -1,3 +1,22 @@
+/**
+ * ownCloud Android client application
+ *
+ * @author David Crespo Ríos
+ * Copyright (C) 2022 ownCloud GmbH.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.owncloud.android.presentation.ui.security.passcode
 
 /**
@@ -11,12 +30,7 @@ interface NumberKeyboardListener {
     fun onNumberClicked(number: Int)
 
     /**
-     * Invoked when the left auxiliary button is clicked.
-     */
-    fun onBiometricButtonClicked()
-
-    /**
-     * Invoked when the right auxiliary button is clicked.
+     * Invoked when the Backspace button is clicked.
      */
     fun onBackspaceButtonClicked()
 }
