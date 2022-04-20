cf65b29c72f35c804ebba803778e3da60f2010de
Abel García de Prada <agdeprada@izertis.com>
2022-04-20 18:55:01 +0200

    Polish fab management. Tags are always shown now.

--- owncloudApp/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.kt
+++ owncloudApp/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.kt
@@ -166,9 +166,6 @@ class FileDisplayActivity : FileActivity(), FileFragment.ContainerActivity, OnEn
     var filesUploadHelper: FilesUploadHelper? = null
         internal set
 
-    private val isFabOpen: Boolean
-        get() = listMainFileFragment?.getFabMain()?.isExpanded ?: false
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -622,7 +619,7 @@ class FileDisplayActivity : FileActivity(), FileFragment.ContainerActivity, OnEn
     }
 
     override fun onBackPressed() {
-        val isFabOpen = isFabOpen
+        val isFabOpen = listMainFileFragment?.isFabExpanded() ?: false
 
         /*
          * BackPressed priority/hierarchy:
@@ -638,7 +635,7 @@ class FileDisplayActivity : FileActivity(), FileFragment.ContainerActivity, OnEn
             super.onBackPressed()
         } else if (!isDrawerOpen() && isFabOpen) {
             // close fab
-            listMainFileFragment?.getFabMain()?.collapse()
+            listMainFileFragment?.collapseFab()
         } else {
             // Every single menu is collapsed. We can navigate up.
             if (secondFragment != null) {
