c966bf14d0f6db273221e5d6a9cfbbdd3ed5482d
Christian Schabesberger <chris.schabesberger@mailbox.org>
2022-04-20 15:12:38 +0200

    add moshi classes for webfinger responses

---
+++ owncloudComLibrary/src/test/java/com/owncloud/android/lib/resources/webfinger/GetOCInstanceViaWebfingerOperationTest.kt
@@ -0,0 +1,11 @@
+package com.owncloud.android.lib.resources.webfinger
+
+import android.os.Build
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.O], manifest = Config.NONE)
+class GetOCInstanceViaWebfingerOperationTest {
+}
