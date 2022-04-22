6bb976b5156a262aa1b9964c35673423d4547953
Abel García de Prada <agdeprada@izertis.com>
2022-04-22 14:54:01 +0200

    Clear multiselection after triggering an operation

--- owncloudApp/src/main/java/com/owncloud/android/presentation/ui/files/filelist/MainFileListFragment.kt
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/ui/files/filelist/MainFileListFragment.kt
@@ -497,12 +497,13 @@ class MainFileListFragment : Fragment(),
                 R.id.action_rename_file -> {
                     val dialog = RenameFileDialogFragment.newInstance(singleFile)
                     dialog.show(requireActivity().supportFragmentManager, FileDetailFragment.FTAG_RENAME_FILE)
+                    fileListAdapter.clearSelection()
+                    updateActionModeAfterTogglingSelected()
                     return true
                 }
                 R.id.action_see_details -> {
-                    if (actionMode != null) {
-                        actionMode!!.finish()
-                    }
+                    fileListAdapter.clearSelection()
+                    updateActionModeAfterTogglingSelected()
                     containerActivity?.showDetails(singleFile)
                     return true
                 }
@@ -534,6 +535,8 @@ class MainFileListFragment : Fragment(),
             R.id.action_remove_file -> {
                 val dialog = RemoveFilesDialogFragment.newInstance(checkedFiles)
                 dialog.show(requireActivity().supportFragmentManager, ConfirmationDialogFragment.FTAG_CONFIRMATION)
+                fileListAdapter.clearSelection()
+                updateActionModeAfterTogglingSelected()
                 return true
             }
             R.id.action_download_file,
