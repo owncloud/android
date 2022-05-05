55861fa17a6108a5ad5657e426bab78c1561857b
Fernando Sanz <fsanz@izertis.com>
2022-05-05 09:39:55 +0200

    Glitch fixed

--- owncloudApp/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ owncloudApp/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -1128,15 +1128,18 @@ public class OCFileListFragment extends ExtendedListFragment implements
             }
 
             if (count == 0) {
-                int emptyMessage;
+                Integer emptyMessage = null;
                 if (mFileListOption == FileListOption.AV_OFFLINE) {
                     emptyMessage = R.string.file_list_empty_available_offline;
                 } else if (mFileListOption == FileListOption.SHARED_BY_LINK) {
                     emptyMessage = R.string.file_list_empty_shared_by_links;
-                } else {
-                    emptyMessage = R.string.file_list_empty;
                 }
-                setMessageForEmptyList(getString(emptyMessage));
+
+                if (emptyMessage != null) {
+                    setMessageForEmptyList(getString(emptyMessage));
+                } else {
+                    setMessageForEmptyList("");
+                }
             }
 
             // decide grid vs list view
