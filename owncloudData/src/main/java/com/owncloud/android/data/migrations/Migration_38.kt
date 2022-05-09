15f0615dfc9e2f3e8232d9edfafb4f389cd0be8f
Abel García de Prada <agdeprada@izertis.com>
2022-05-09 09:16:43 +0200

    Fix some errors after rebasing

--- owncloudData/src/main/java/com/owncloud/android/data/migrations/Migration_38.kt
+++ owncloudData/src/main/java/com/owncloud/android/data/migrations/Migration_38.kt
@@ -21,7 +21,7 @@ package com.owncloud.android.data.migrations
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
-val MIGRATION_36_37 = object : Migration(36, 37) {
+val MIGRATION_37_38 = object : Migration(37, 38) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("CREATE TABLE IF NOT EXISTS `files` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `parentId` INTEGER, `owner` TEXT NOT NULL, `remotePath` TEXT NOT NULL, `remoteId` TEXT, `length` INTEGER NOT NULL, `creationTimestamp` INTEGER, `modificationTimestamp` INTEGER NOT NULL, `mimeType` TEXT NOT NULL, `etag` TEXT, `permissions` TEXT, `privateLink` TEXT, `storagePath` TEXT, `name` TEXT, `treeEtag` TEXT, `keepInSync` INTEGER, `lastSyncDateForData` INTEGER, `fileShareViaLink` INTEGER, `lastSyncDateForProperties` INTEGER, `needsToUpdateThumbnail` INTEGER NOT NULL, `modifiedAtLastSyncForData` INTEGER, `etagInConflict` TEXT, `fileIsDownloading` INTEGER, `sharedWithSharee` INTEGER, `sharedByLink` INTEGER NOT NULL)")
     }
