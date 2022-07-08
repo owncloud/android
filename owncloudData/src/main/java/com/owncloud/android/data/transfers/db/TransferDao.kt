9bfd9e5919c55922dd4a6f99800c91a25ddb0272
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2022-07-08 11:29:00 +0200

    Created LocalTransferDataSource implementation in data layer and added some methods to DAO

--- owncloudData/src/main/java/com/owncloud/android/data/transfers/db/TransferDao.kt
+++ owncloudData/src/main/java/com/owncloud/android/data/transfers/db/TransferDao.kt
@@ -28,8 +28,14 @@ import com.owncloud.android.data.ProviderMeta.ProviderTableMeta.TRANSFERS_TABLE_
 
 @Dao
 abstract class TransferDao {
-    @Query(SELECT_TRANSFER_WITH_ID)
-    abstract fun getTransferById(id: Long): OCTransferEntity?
+    @Query(SELECT_LAST_TRANSFER_WITH_REMOTE_PATH_AND_ACCOUNT_NAME)
+    abstract fun getLastTransferWithRemotePathAndAccountName(remotePath: String, accountName: String): OCTransferEntity?
+
+    @Query(SELECT_TRANSFERS_WITH_STATUS)
+    abstract fun getTransfersWithStatus(status: List<Int>): List<OCTransferEntity>
+
+    @Query(SELECT_ALL_TRANSFERS)
+    abstract fun getAllTransfers(): List<OCTransferEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract fun insert(ocTransferEntity: OCTransferEntity): Long
@@ -37,14 +43,39 @@ abstract class TransferDao {
     @Query(DELETE_TRANSFER_WITH_ID)
     abstract fun deleteTransferWithId(id: Long)
 
+    @Query(DELETE_TRANSFERS_WITH_ACCOUNT_NAME)
+    abstract fun deleteTransfersWithAccountName(accountName: String)
+
+    @Query(DELETE_TRANSFERS_WITH_STATUS)
+    abstract fun deleteTransfersWithStatus(status: Int)
+
     companion object {
-        private const val SELECT_TRANSFER_WITH_ID =
+        private const val SELECT_LAST_TRANSFER_WITH_REMOTE_PATH_AND_ACCOUNT_NAME =
             "SELECT * " +
                     "FROM $TRANSFERS_TABLE_NAME " +
-                    "WHERE id = :id"
+                    "WHERE remotePath = :remotePath AND accountName = :accountName " +
+                    "ORDER BY transferEndTimestamp DESC " +
+                    "LIMIT 1"
+
+        private const val SELECT_TRANSFERS_WITH_STATUS =
+            "SELECT * " +
+                    "FROM $TRANSFERS_TABLE_NAME " +
+                    "WHERE status IN (:status)"
+
+        private const val SELECT_ALL_TRANSFERS =
+            "SELECT * " +
+                    "FROM $TRANSFERS_TABLE_NAME "
 
         private const val DELETE_TRANSFER_WITH_ID =
             "DELETE FROM $TRANSFERS_TABLE_NAME " +
                     "WHERE id = :id"
+
+        private const val DELETE_TRANSFERS_WITH_ACCOUNT_NAME =
+            "DELETE FROM $TRANSFERS_TABLE_NAME " +
+                    "WHERE accountName = :accountName"
+
+        private const val DELETE_TRANSFERS_WITH_STATUS =
+            "DELETE FROM $TRANSFERS_TABLE_NAME " +
+                    "WHERE status = :status"
     }
 }
