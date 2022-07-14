1aaff60efd0e69de819a44b42cc4f1129516c7db
Juan Carlos Garrote <57049315+JuancaG05@users.noreply.github.com>
2022-07-14 09:51:06 +0200

    Move CancelUploadForFileUseCase to new arch

--- owncloudApp/src/main/java/com/owncloud/android/usecases/transfers/uploads/CancelUploadForFileUseCase.kt
+++ owncloudApp/src/main/java/com/owncloud/android/usecases/transfers/uploads/CancelUploadForFileUseCase.kt
@@ -2,7 +2,9 @@
  * ownCloud Android client application
  *
  * @author Abel García de Prada
- * Copyright (C) 2021 ownCloud GmbH.
+ * @author Juan Carlos Garrote Gascón
+ *
+ * Copyright (C) 2022 ownCloud GmbH.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -16,13 +18,14 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.owncloud.android.usecases.transfers.uploads
 
 import androidx.work.WorkManager
 import com.owncloud.android.MainApp
-import com.owncloud.android.datamodel.UploadsStorageManager
 import com.owncloud.android.domain.BaseUseCase
 import com.owncloud.android.domain.files.model.OCFile
+import com.owncloud.android.domain.transfers.TransferRepository
 import com.owncloud.android.extensions.getWorkInfoByTags
 import com.owncloud.android.workers.UploadFileFromContentUriWorker.Companion.TRANSFER_TAG_MANUAL_UPLOAD
 import timber.log.Timber
@@ -32,16 +35,16 @@ import timber.log.Timber
  * policy and work that is already executing may continue to run.
  */
 class CancelUploadForFileUseCase(
-    private val workManager: WorkManager
+    private val workManager: WorkManager,
+    private val transferRepository: TransferRepository,
 ) : BaseUseCase<Unit, CancelUploadForFileUseCase.Params>() {
 
     override fun run(params: Params) {
         val file = params.file
-        val uploadsStorageManager = UploadsStorageManager(MainApp.appContext.contentResolver)
 
         // Check if there are pending uploads for this file.
         // FirstOrNull because it should not be 2 uploads with same owner and remote path at the same time
-        val uploadForFile = uploadsStorageManager.currentAndPendingUploads.firstOrNull {
+        val uploadForFile = transferRepository.getCurrentAndPendingTransfers().firstOrNull {
             file.owner == it.accountName && file.remotePath == it.remotePath
         }
 
@@ -51,7 +54,7 @@ class CancelUploadForFileUseCase(
         }
 
         val workersToCancel =
-            workManager.getWorkInfoByTags(listOf(TRANSFER_TAG_MANUAL_UPLOAD, uploadForFile.uploadId.toString(), file.owner))
+            workManager.getWorkInfoByTags(listOf(TRANSFER_TAG_MANUAL_UPLOAD, uploadForFile.id.toString(), file.owner))
 
         workersToCancel.forEach {
             // TODO: We need to check if the work is cancelled before finishing.
