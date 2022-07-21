e109059a851048e877d124aa625a3fd48e9a1c7f
Abel García de Prada <agdeprada@izertis.com>
2022-07-21 13:29:06 +0200

    Replace old livedata state holder with a new stateflow that combines account, folder, filelistoption and searchfilter

--- owncloudApp/src/main/java/com/owncloud/android/extensions/FragmentExt.kt
+++ owncloudApp/src/main/java/com/owncloud/android/extensions/FragmentExt.kt
@@ -24,7 +24,13 @@ import android.content.Context
 import android.content.DialogInterface
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 fun Fragment.showErrorInSnackbar(genericErrorMessageId: Int, throwable: Throwable?) =
     throwable?.let {
@@ -65,5 +71,17 @@ fun Fragment.hideSoftKeyboard() {
             focusedView.windowToken,
             0
         )
+    }
+}
+
+fun <T> Fragment.collectLatestLifecycleFlow(
+    flow: Flow<T>,
+    lifecycleState: Lifecycle.State = Lifecycle.State.STARTED,
+    collect: suspend (T) -> Unit
+) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(lifecycleState) {
+            flow.collectLatest(collect)
+        }
     }
 }
---
+++ owncloudDomain/src/main/java/com/owncloud/android/domain/availableoffline/usecases/GetFilesAvailableOfflineFromAccountAsStreamUseCase.kt
@@ -0,0 +1,34 @@
+/*
+ * ownCloud Android client application
+ *
+ * Copyright (C) 2021 ownCloud GmbH.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.owncloud.android.domain.availableoffline.usecases
+
+import com.owncloud.android.domain.BaseUseCase
+import com.owncloud.android.domain.files.FileRepository
+import com.owncloud.android.domain.files.model.OCFile
+import kotlinx.coroutines.flow.Flow
+
+class GetFilesAvailableOfflineFromAccountAsStreamUseCase(
+    private val fileRepository: FileRepository
+) : BaseUseCase<Flow<List<OCFile>>, GetFilesAvailableOfflineFromAccountAsStreamUseCase.Params>() {
+
+    override fun run(params: Params): Flow<List<OCFile>> = fileRepository.getFilesAvailableOfflineFromAccountAsStream(params.owner)
+
+    data class Params(
+        val owner: String
+    )
+}
