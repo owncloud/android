b3faa364488f088cc484c51a161b8b592eb79dd4
Abel García de Prada <agdeprada@izertis.com>
2022-08-16 08:55:41 +0200

    Apply some tiny suggestions from CR

--- owncloudDomain/src/test/java/com/owncloud/android/domain/files/usecases/GetSharedByLinkForAccountAsStreamUseCaseTest.kt
+++ owncloudDomain/src/test/java/com/owncloud/android/domain/files/usecases/GetSharedByLinkForAccountAsStreamUseCaseTest.kt
@@ -2,7 +2,7 @@
  * ownCloud Android client application
  *
  * @author Fernando Sanz Velasco
- * Copyright (C) 2021 ownCloud GmbH.
+ * Copyright (C) 2022 ownCloud GmbH.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
