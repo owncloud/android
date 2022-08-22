8fe440480f87a9ba91591e90dd2736857e8c0c13
Juan Carlos Garrote <57049315+JuancaG05@users.noreply.github.com>
2022-08-22 15:02:58 +0200

    Moved removal of temporal unnecessary files to LocalStorageProvider

--- owncloudDomain/src/main/java/com/owncloud/android/domain/transfers/usecases/GetAllTransfersUseCase.kt
+++ owncloudDomain/src/main/java/com/owncloud/android/domain/transfers/usecases/GetAllTransfersUseCase.kt
@@ -4,16 +4,16 @@
  * @author Juan Carlos Garrote Gascón
  *
  * Copyright (C) 2022 ownCloud GmbH.
- * <p>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
  * as published by the Free Software Foundation.
- * <p>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * <p>
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,22 +23,11 @@ package com.owncloud.android.domain.transfers.usecases
 import com.owncloud.android.domain.BaseUseCase
 import com.owncloud.android.domain.transfers.TransferRepository
 import com.owncloud.android.domain.transfers.model.OCTransfer
-import java.io.File
 
-class UpdatePendingUploadsPathUseCase(
+class GetAllTransfersUseCase(
     private val transferRepository: TransferRepository,
-) : BaseUseCase<Unit, UpdatePendingUploadsPathUseCase.Params>() {
+) : BaseUseCase<List<OCTransfer>, Unit>() {
+    override fun run(params: Unit): List<OCTransfer> =
+        transferRepository.getAllTransfers()
 
-    override fun run(params: Params) {
-        transferRepository.clearSuccessfulTransfers()
-        val storedUploads = transferRepository.getAllTransfers()
-        storedUploads.forEach { upload ->
-            transferRepository.updateTransferStorageDirectoryInLocalPath(upload.id!!, params.oldDirectory, params.newDirectory)
-        }
-    }
-
-    data class Params(
-        val oldDirectory: String,
-        val newDirectory: String
-    )
 }
