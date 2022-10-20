8fc527b95cb9ea6ec50ffdef5b118cba02c7e88a
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2022-10-20 08:35:13 +0200

    Remove unnecessary dismissLoadingDialog()

--- owncloudApp/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.kt
+++ owncloudApp/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.kt
@@ -1063,8 +1063,6 @@ class FileDisplayActivity : FileActivity(),
                 // Not blocking the UI
             }
             is UIResult.Success -> {
-                dismissLoadingDialog()
-
                 val listOfFilesRemoved = uiResult.data ?: return
                 val lastRemovedFile = listOfFilesRemoved.last()
                 val singleRemoval = listOfFilesRemoved.size == 1
@@ -1093,8 +1091,6 @@ class FileDisplayActivity : FileActivity(),
                 invalidateOptionsMenu()
             }
             is UIResult.Error -> {
-                dismissLoadingDialog()
-
                 showErrorInSnackbar(R.string.remove_fail_msg, uiResult.getThrowableOrNull())
 
                 if (uiResult.getThrowableOrNull() is SSLRecoverablePeerUnverifiedException) {
