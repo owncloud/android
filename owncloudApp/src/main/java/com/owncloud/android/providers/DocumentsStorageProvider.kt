3b8ff3b766e621699daee3fdcdfed6a70fcbfd56
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2022-11-16 16:57:38 +0100

    Fixed upload path for uploads from document provider

--- owncloudApp/src/main/java/com/owncloud/android/providers/DocumentsStorageProvider.kt
+++ owncloudApp/src/main/java/com/owncloud/android/providers/DocumentsStorageProvider.kt
@@ -131,7 +131,7 @@ class DocumentsStorageProvider : DocumentsProvider() {
                     val uploadFilesUseCaseParams = UploadFilesFromSystemUseCase.Params(
                         accountName = ocFile.owner,
                         listOfLocalPaths = listOf(fileToOpen.path),
-                        uploadFolderPath = ocFile.remotePath.substringBeforeLast(PATH_SEPARATOR)
+                        uploadFolderPath = ocFile.remotePath.substringBeforeLast(PATH_SEPARATOR).plus(PATH_SEPARATOR)
                     )
                     CoroutineScope(Dispatchers.IO).launch {
                         uploadFilesUseCase.execute(uploadFilesUseCaseParams)
@@ -235,7 +235,7 @@ class DocumentsStorageProvider : DocumentsProvider() {
 
         val file = getFileByIdOrException(documentId.toInt())
 
-        val realFile = File(file?.storagePath)
+        val realFile = File(file.storagePath)
 
         return AssetFileDescriptor(
             ParcelFileDescriptor.open(realFile, ParcelFileDescriptor.MODE_READ_ONLY), 0, AssetFileDescriptor.UNKNOWN_LENGTH
