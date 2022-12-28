d765143a3705316753e87f67291c49eafa97da2d
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2022-12-28 15:11:21 +0100

    Added usecase for retrieving project spaces as Flow from DB

--- owncloudData/src/main/java/com/owncloud/android/data/spaces/datasources/LocalSpacesDataSource.kt
+++ owncloudData/src/main/java/com/owncloud/android/data/spaces/datasources/LocalSpacesDataSource.kt
@@ -2,6 +2,8 @@
  * ownCloud Android client application
  *
  * @author Abel García de Prada
+ * @author Juan Carlos Garrote Gascón
+ *
  * Copyright (C) 2022 ownCloud GmbH.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,10 +18,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.owncloud.android.data.spaces.datasources
 
 import com.owncloud.android.domain.spaces.model.OCSpace
+import kotlinx.coroutines.flow.Flow
 
 interface LocalSpacesDataSource {
     fun saveSpacesForAccount(listOfSpaces: List<OCSpace>)
+    fun getProjectSpacesForAccountAsFlow(accountName: String): Flow<List<OCSpace>>
 }
