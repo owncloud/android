55b3b4cca7807426634b301a2a8a7aa1089da234
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2023-01-10 17:28:21 +0100

    Added DiffUtil to spaces list adapter

---
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/spaces/SpacesListDiffUtil.kt
@@ -0,0 +1,52 @@
+/**
+ * ownCloud Android client application
+ *
+ * @author Juan Carlos Garrote Gascón
+ *
+ * Copyright (C) 2023 ownCloud GmbH.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.owncloud.android.presentation.spaces
+
+import androidx.recyclerview.widget.DiffUtil
+import com.owncloud.android.domain.spaces.model.OCSpace
+
+class SpacesListDiffUtil(
+    private val oldList: List<OCSpace>,
+    private val newList: List<OCSpace>
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+
+        if ((oldItem.name != newItem.name) || (oldItem.description != newItem.description) ||
+            (oldItem.getSpaceSpecialImage()?.id != newItem.getSpaceSpecialImage()?.id)) {
+            return false
+        }
+
+        return true
+    }
+}
