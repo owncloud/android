e86e1634ea708c5dbe73b81e9994bd6efbe1c729
Jesus Recio <jrecio@izertis.com>
2023-02-21 17:31:11 +0100

    polish releases notes grouping all 3.x features for oem release

--- owncloudApp/src/main/java/com/owncloud/android/presentation/viewmodels/releasenotes/ReleaseNotesViewModel.kt
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/viewmodels/releasenotes/ReleaseNotesViewModel.kt
@@ -47,9 +47,13 @@ class ReleaseNotesViewModel(
 
     companion object {
         val releaseNotesList = listOf(
-            ReleaseNote(R.string.release_notes_3_0_3_title1, R.string.release_notes_3_0_3_subtitle1, ReleaseNoteType.BUGFIX),
-            ReleaseNote(R.string.release_notes_3_0_3_title2, R.string.release_notes_3_0_3_subtitle2, ReleaseNoteType.BUGFIX),
-            ReleaseNote(R.string.release_notes_3_0_3_title3, R.string.release_notes_3_0_3_subtitle3, ReleaseNoteType.BUGFIX)
+            ReleaseNote(R.string.release_notes_3_0_title1, R.string.release_notes_3_0_subtitle1, ReleaseNoteType.ENHANCEMENT),
+            ReleaseNote(R.string.release_notes_3_0_title2, R.string.release_notes_3_0_subtitle2, ReleaseNoteType.ENHANCEMENT),
+            ReleaseNote(R.string.release_notes_3_0_title5, R.string.release_notes_3_0_subtitle5, ReleaseNoteType.ENHANCEMENT),
+            ReleaseNote(R.string.release_notes_3_0_3_title1, R.string.release_notes_3_0_3_subtitle1, ReleaseNoteType.ENHANCEMENT),
+            ReleaseNote(R.string.release_notes_3_0_title4, R.string.release_notes_3_0_subtitle4, ReleaseNoteType.CHANGE),
+            ReleaseNote(R.string.release_notes_3_0_title3, R.string.release_notes_3_0_subtitle3, ReleaseNoteType.BUGFIX),
+            ReleaseNote(R.string.release_notes_3_0_2_title1, R.string.release_notes_3_0_2_subtitle1, ReleaseNoteType.BUGFIX),
         )
     }
 }
