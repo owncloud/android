218bd6fcdcdc7176de2a24dec1d4b084c38093e9
Abel García de Prada <agdeprada@izertis.com>
2023-03-07 11:50:22 +0100

    Add release notes for v3.0.4

--- owncloudApp/src/main/java/com/owncloud/android/presentation/viewmodels/releasenotes/ReleaseNotesViewModel.kt
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/viewmodels/releasenotes/ReleaseNotesViewModel.kt
@@ -47,9 +47,8 @@ class ReleaseNotesViewModel(
 
     companion object {
         val releaseNotesList = listOf(
-            ReleaseNote(R.string.release_notes_3_0_3_title1, R.string.release_notes_3_0_3_subtitle1, ReleaseNoteType.BUGFIX),
-            ReleaseNote(R.string.release_notes_3_0_3_title2, R.string.release_notes_3_0_3_subtitle2, ReleaseNoteType.BUGFIX),
-            ReleaseNote(R.string.release_notes_3_0_3_title3, R.string.release_notes_3_0_3_subtitle3, ReleaseNoteType.BUGFIX)
+            ReleaseNote(R.string.release_notes_3_0_4_title1, R.string.release_notes_3_0_4_subtitle1, ReleaseNoteType.SECURITY),
+            ReleaseNote(R.string.release_notes_3_0_4_title2, R.string.release_notes_3_0_4_subtitle2, ReleaseNoteType.CHANGE),
         )
     }
 }
