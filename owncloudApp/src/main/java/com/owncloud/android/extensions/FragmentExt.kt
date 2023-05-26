4ecddb8f4fc50b39114b9627772b0ec2810b9d87
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2023-05-26 09:50:43 +0200

    Created needed extensions to filter menu options

--- owncloudApp/src/main/java/com/owncloud/android/extensions/FragmentExt.kt
+++ owncloudApp/src/main/java/com/owncloud/android/extensions/FragmentExt.kt
@@ -2,7 +2,9 @@
  * ownCloud Android client application
  *
  * @author David González Verdugo
- * Copyright (C) 2020 ownCloud GmbH.
+ * @author Juan Carlos Garrote Gascón
+ *
+ * Copyright (C) 2023 ownCloud GmbH.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -22,12 +24,15 @@ package com.owncloud.android.extensions
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
+import android.view.Menu
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.google.android.material.snackbar.Snackbar
+import com.owncloud.android.R
+import com.owncloud.android.domain.files.model.FileMenuOption
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -83,5 +88,32 @@ fun <T> Fragment.collectLatestLifecycleFlow(
         repeatOnLifecycle(lifecycleState) {
             flow.collectLatest(collect)
         }
+    }
+}
+
+fun Fragment.filterMenuOptions(
+    menu: Menu,
+    optionsToShow: List<FileMenuOption>,
+    hasWritePermission: Boolean,
+) {
+    FileMenuOption.values().forEach { fileMenuOption ->
+        val item = menu.findItem(fileMenuOption.toResId())
+        item?.let {
+            if (optionsToShow.contains(fileMenuOption)) {
+                it.isVisible = true
+                it.isEnabled = true
+                if (fileMenuOption.toResId() == R.id.action_open_file_with) {
+                    if (!hasWritePermission) {
+                        item.setTitle(R.string.actionbar_open_with_read_only)
+                    } else {
+                        item.setTitle(R.string.actionbar_open_with)
+                    }
+                }
+            } else {
+                it.isVisible = false
+                it.isEnabled = false
+            }
+        }
+
     }
 }
