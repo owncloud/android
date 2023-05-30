b5e5c864259038690b5cceb2db0ab13384d2aecd
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2023-05-30 09:33:04 +0200

    Use new filter use case in text preview screen

---
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/previews/PreviewTextViewModel.kt
@@ -0,0 +1,70 @@
+/**
+ * ownCloud Android client application
+ *
+ * @author Juan Carlos Garrote Gascón
+ *
+ * Copyright (C) 2023 ownCloud GmbH.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.owncloud.android.presentation.previews
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.owncloud.android.domain.files.model.FileMenuOption
+import com.owncloud.android.domain.files.model.OCFile
+import com.owncloud.android.providers.CoroutinesDispatcherProvider
+import com.owncloud.android.usecases.files.FilterFileMenuOptionsUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class PreviewTextViewModel(
+    private val filterFileMenuOptionsUseCase: FilterFileMenuOptionsUseCase,
+    private val coroutinesDispatcherProvider: CoroutinesDispatcherProvider,
+) : ViewModel() {
+
+    private val _menuOptions: MutableStateFlow<List<FileMenuOption>> = MutableStateFlow(emptyList())
+    val menuOptions: StateFlow<List<FileMenuOption>> = _menuOptions
+
+    fun filterMenuOptions(
+        file: OCFile, accountName: String, isAnyFileVideoPreviewing: Boolean, shareViaLinkAllowed: Boolean,
+        shareWithUsersAllowed: Boolean, sendAllowed: Boolean
+    ) {
+        viewModelScope.launch(coroutinesDispatcherProvider.io) {
+            val result = filterFileMenuOptionsUseCase.execute(
+                FilterFileMenuOptionsUseCase.Params(
+                    files = listOf(file),
+                    accountName = accountName,
+                    isAnyFileVideoPreviewing = isAnyFileVideoPreviewing,
+                    displaySelectAll = false,
+                    displaySelectInverse = false,
+                    onlyAvailableOfflineFiles = false,
+                    onlySharedByLinkFiles = false,
+                    shareViaLinkAllowed = shareViaLinkAllowed,
+                    shareWithUsersAllowed = shareWithUsersAllowed,
+                    sendAllowed = sendAllowed,
+                )
+            )
+            result.apply {
+                remove(FileMenuOption.RENAME)
+                remove(FileMenuOption.MOVE)
+                remove(FileMenuOption.COPY)
+                remove(FileMenuOption.SYNC)
+            }
+            _menuOptions.update { result }
+        }
+    }
+}
