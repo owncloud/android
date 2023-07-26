00776f6e33925c719dfc9f30c4e0c5b4f3a15669
Manuel Plazas Palacio <manuelplazaspalacio@gmail.com>
2023-07-26 13:37:31 +0200

    Testing the http enforced.

--- owncloudDomain/src/test/java/com/owncloud/android/domain/server/usecases/GetServerInfoAsyncUseCaseTest.kt
+++ owncloudDomain/src/test/java/com/owncloud/android/domain/server/usecases/GetServerInfoAsyncUseCaseTest.kt
@@ -18,8 +18,10 @@
  */
 package com.owncloud.android.domain.server.usecases
 
+import com.owncloud.android.domain.exceptions.SSLErrorException
 import com.owncloud.android.domain.server.ServerInfoRepository
 import com.owncloud.android.domain.server.usecases.GetServerInfoAsyncUseCase.Companion.TRAILING_SLASH
+import com.owncloud.android.testutil.OC_INSECURE_SERVER_INFO_BASIC_AUTH
 import com.owncloud.android.testutil.OC_SECURE_SERVER_INFO_BASIC_AUTH
 import io.mockk.every
 import io.mockk.spyk
@@ -32,7 +34,11 @@ class GetServerInfoAsyncUseCaseTest {
 
     private val repository: ServerInfoRepository = spyk()
     private val useCase = GetServerInfoAsyncUseCase((repository))
-    private val useCaseParams = GetServerInfoAsyncUseCase.Params(serverPath = "http://demo.owncloud.com", false)
+    private val useCaseParams = GetServerInfoAsyncUseCase.Params(
+        serverPath = "http://demo.owncloud.com",
+        creatingAccount = false,
+        secureConnectionEnforced = false,
+    )
     private val useCaseParamsWithSlash = useCaseParams.copy(serverPath = useCaseParams.serverPath.plus(TRAILING_SLASH))
 
     @Test
@@ -70,4 +76,29 @@ class GetServerInfoAsyncUseCaseTest {
 
         verify(exactly = 1) { repository.getServerInfo(useCaseParams.serverPath, false) }
     }
+
+    @Test
+    fun `Should throw SSLErrorException when secureConnectionEnforced is true and ServerInfoRepository returns ServerInfo with isSecureConnection returning false`(){
+        every { repository.getServerInfo(useCaseParams.serverPath, false) }  returns OC_INSECURE_SERVER_INFO_BASIC_AUTH
+
+        val useCaseResult = useCase.execute(useCaseParams.copy(secureConnectionEnforced = true))
+
+        assertTrue(useCaseResult.isError)
+        assertTrue(useCaseResult.getThrowableOrNull() is SSLErrorException)
+
+        verify(exactly = 1) { repository.getServerInfo(useCaseParams.serverPath, false) }
+    }
+
+    @Test
+    fun `Should work correctly when secureConnectionEnforced is true and ServerInfoRepository returns ServerInfo with isSecureConnection returning true`(){
+        every { repository.getServerInfo(useCaseParams.serverPath, false) }  returns OC_SECURE_SERVER_INFO_BASIC_AUTH
+
+        val useCaseResult = useCase.execute(useCaseParams.copy(secureConnectionEnforced = true))
+
+        assertTrue(useCaseResult.isSuccess)
+        assertEquals(OC_SECURE_SERVER_INFO_BASIC_AUTH, useCaseResult.getDataOrNull())
+
+        verify(exactly = 1) { repository.getServerInfo(useCaseParams.serverPath, false) }
+    }
+
 }
