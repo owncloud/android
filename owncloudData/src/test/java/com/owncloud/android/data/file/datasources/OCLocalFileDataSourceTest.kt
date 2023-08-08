1eba0a7d8d0631a93680e0b684b4c1a869043098
Aitorbp <aitor.ballesteros.pavon@gmail.com>
2023-08-08 13:25:05 +0100

    Working with LocalFileDataSourceTest

--- owncloudData/src/test/java/com/owncloud/android/data/file/datasources/OCLocalFileDataSourceTest.kt
+++ owncloudData/src/test/java/com/owncloud/android/data/file/datasources/OCLocalFileDataSourceTest.kt
@@ -47,6 +47,12 @@ import org.junit.Test
 class OCLocalFileDataSourceTest {
     private lateinit var localDataSource: OCLocalFileDataSource
     private lateinit var dao: FileDao
+    private val ocFileAndFileSync = OCFileAndFileSync(OC_FILE.toEntity(), OCFileSyncEntity(
+        fileId = OC_FILE.id!!,
+        uploadWorkerUuid = null,
+        downloadWorkerUuid = null,
+        isSynchronizing = false
+    ), OC_SPACE_PERSONAL.toEntity())
 
     @Before
     fun init() {
@@ -55,42 +61,69 @@ class OCLocalFileDataSourceTest {
     }
 
     @Test
-    fun `get file by id - ok`() {
+    fun `getFileById returns ok`() {
         every { dao.getFileById(any()) } returns DUMMY_FILE_ENTITY
 
         val result = localDataSource.getFileById(OC_FILE.id!!)
 
         assertEquals(OC_FILE, result)
 
-        verify { dao.getFileById(OC_FILE.id!!) }
+        verify (exactly = 1) { dao.getFileById(OC_FILE.id!!) }
     }
 
     @Test
-    fun `get file by id - ok - null`() {
+    fun `getFileById returns null`() {
         every { dao.getFileById(any()) } returns null
 
         val result = localDataSource.getFileById(DUMMY_FILE_ENTITY.id)
 
         assertNull(result)
 
-        verify { dao.getFileById(DUMMY_FILE_ENTITY.id) }
+        verify (exactly = 1) { dao.getFileById(DUMMY_FILE_ENTITY.id) }
     }
 
     @Test(expected = Exception::class)
-    fun `get file by id - ko`() {
+    fun `getFileById returns an Exception`() {
         every { dao.getFileById(any()) } throws Exception()
 
         localDataSource.getFileById(DUMMY_FILE_ENTITY.id)
     }
 
     @Test
+    fun `getFileByIdAsFlow returns ok`() =  runBlocking {
+        every { dao.getFileByIdAsFlow(any()) } returns flowOf(DUMMY_FILE_ENTITY)
+
+        val result = localDataSource.getFileByIdAsFlow(OC_FILE.id!!)
+
+        result.collect{ result ->
+            assertEquals(OC_FILE, result)
+        }
+
+        verify (exactly = 1) { dao.getFileByIdAsFlow(OC_FILE.id!!) }
+    }
+
+    @Test
+    fun `getFileByIdAsFlow returns null`() =  runBlocking {
+        every { dao.getFileByIdAsFlow(any()) } returns flowOf(null)
+
+        val result = localDataSource.getFileByIdAsFlow(DUMMY_FILE_ENTITY.id)
+
+        result.collect { result->
+            assertNull(result)
+        }
+
+        verify (exactly = 1) { dao.getFileByIdAsFlow(DUMMY_FILE_ENTITY.id) }
+    }
+
+    @Test(expected = Exception::class)
+    fun `getFileByIdAsFlow returns an Exception`() {
+        every { dao.getFileByIdAsFlow(any()) } throws Exception()
+
+        localDataSource.getFileByIdAsFlow(DUMMY_FILE_ENTITY.id)
+    }
+
+    @Test
     fun `getFileWithSyncInfoByIdAsFlow returns a flow of OCFileWithSyncInfo object`() = runBlocking {
-        val ocFileAndFileSync = OCFileAndFileSync(OC_FILE.toEntity(), OCFileSyncEntity(
-            fileId = OC_FILE.id!!,
-            uploadWorkerUuid = null,
-            downloadWorkerUuid = null,
-            isSynchronizing = false
-        ), OC_SPACE_PERSONAL.toEntity())
 
         every { dao.getFileWithSyncInfoByIdAsFlow(OC_FILE.id!!) } returns flowOf(ocFileAndFileSync)
 
@@ -100,7 +133,7 @@ class OCLocalFileDataSourceTest {
             assertEquals(OC_FILE_WITH_SYNC_INFO_AND_SPACE, emittedFileWithSyncInfo)
         }
 
-        verify { dao.getFileWithSyncInfoByIdAsFlow(OC_FILE.id!!) }
+        verify (exactly = 1) { dao.getFileWithSyncInfoByIdAsFlow(OC_FILE.id!!) }
     }
 
     @Test
@@ -114,11 +147,11 @@ class OCLocalFileDataSourceTest {
             assertNull(emittedFileWithSyncInfo)
         }
 
-        verify { dao.getFileWithSyncInfoByIdAsFlow(OC_FILE.id!!) }
+        verify (exactly = 1) { dao.getFileWithSyncInfoByIdAsFlow(OC_FILE.id!!) }
     }
 
     @Test(expected = Exception::class)
-    fun `getFileWithSyncInfoByIdAsFlow returns a Expection when DAO receive a Exception`() = runBlocking {
+    fun `getFileWithSyncInfoByIdAsFlow returns an exception when DAO receive a Exception`() = runBlocking {
 
         every { dao.getFileWithSyncInfoByIdAsFlow(OC_FILE.id!!) } throws Exception()
 
@@ -128,33 +161,33 @@ class OCLocalFileDataSourceTest {
             assertEquals(OC_FILE_WITH_SYNC_INFO_AND_SPACE, emittedFileWithSyncInfo)
         }
 
-        verify { dao.getFileWithSyncInfoByIdAsFlow(OC_FILE.id!!) }
+        verify (exactly = 1) { dao.getFileWithSyncInfoByIdAsFlow(OC_FILE.id!!) }
     }
 
     @Test
-    fun `get file by remote path - ok`() {
+    fun `getFileByRemotePath returns the OCFIle`() {
         every { dao.getFileByOwnerAndRemotePath(any(), any(), any()) } returns DUMMY_FILE_ENTITY
 
         val result = localDataSource.getFileByRemotePath(OC_FILE.remotePath, OC_FILE.owner, OC_FILE.spaceId)
 
         assertEquals(OC_FILE, result)
 
-        verify { dao.getFileByOwnerAndRemotePath(OC_FILE.owner, OC_FILE.remotePath, OC_FILE.spaceId) }
+        verify (exactly = 1) { dao.getFileByOwnerAndRemotePath(OC_FILE.owner, OC_FILE.remotePath, OC_FILE.spaceId) }
     }
 
     @Test
-    fun `get file by remote path - ok - null`() {
+    fun `getFileByRemotePath returns null when DAO is null`() {
         every { dao.getFileByOwnerAndRemotePath(any(), any(), any()) } returns null
 
         val result = localDataSource.getFileByRemotePath(OC_FILE.remotePath, OC_FILE.owner, OC_FILE.spaceId)
 
         assertNull(result)
 
-        verify { dao.getFileByOwnerAndRemotePath(OC_FILE.owner, OC_FILE.remotePath, OC_FILE.spaceId) }
+        verify (exactly = 1) { dao.getFileByOwnerAndRemotePath(OC_FILE.owner, OC_FILE.remotePath, OC_FILE.spaceId) }
     }
 
     @Test
-    fun `get file by remote path - ok - null - create root folder`() {
+    fun `getFileByRemotePath returns null when create root folder`() {
         every { dao.getFileByOwnerAndRemotePath(any(), any(), any()) } returns null
         every { dao.mergeRemoteAndLocalFile(any()) } returns 1234
         every { dao.getFileById(1234) } returns DUMMY_FILE_ENTITY.copy(
@@ -171,7 +204,7 @@ class OCLocalFileDataSourceTest {
         assertEquals(MIME_DIR, result.mimeType)
         assertEquals(ROOT_PATH, result.remotePath)
 
-        verify {
+        verify (exactly = 1) {
             dao.getFileByOwnerAndRemotePath(OC_FILE.owner, ROOT_PATH, null)
             dao.mergeRemoteAndLocalFile(any())
             dao.getFileById(1234)
@@ -179,16 +212,48 @@ class OCLocalFileDataSourceTest {
     }
 
     @Test(expected = Exception::class)
-    fun `get file by remote path - ko`() {
+    fun `getFileByRemotePath returns an exception`() {
         every { dao.getFileByOwnerAndRemotePath(any(), any(), any()) } throws Exception()
 
         localDataSource.getFileByRemotePath(OC_FILE.remotePath, OC_FILE.owner, null)
 
-        verify { dao.getFileByOwnerAndRemotePath(OC_FILE.owner, OC_FILE.remotePath, null) }
+        verify (exactly = 1) { dao.getFileByOwnerAndRemotePath(OC_FILE.owner, OC_FILE.remotePath, null) }
     }
 
     @Test
-    fun `get folder content - ok`() {
+    fun `getFileByRemoteId returns OCFile`() {
+        every { dao.getFileByRemoteId(any()) } returns DUMMY_FILE_ENTITY
+
+        val result = localDataSource.getFileByRemoteId(DUMMY_FILE_ENTITY.remoteId.toString())
+
+        assertEquals(OC_FILE, result)
+
+        verify (exactly = 1) { dao.getFileByRemoteId(DUMMY_FILE_ENTITY.remoteId.toString()) }
+    }
+
+    @Test
+    fun `getFileByRemoteId returns null when DAO is null`() {
+        every { dao.getFileByRemoteId(any()) } returns null
+
+        val result = localDataSource.getFileByRemoteId(DUMMY_FILE_ENTITY.remoteId.toString())
+
+        assertEquals(null, result)
+
+        verify (exactly = 1) { dao.getFileByRemoteId(DUMMY_FILE_ENTITY.remoteId.toString()) }
+    }
+
+
+    @Test(expected = Exception::class)
+    fun `getFileByRemoteId returns an exception`() {
+        every { dao.getFileByRemoteId(any()) } throws Exception()
+
+        localDataSource.getFileByRemoteId(DUMMY_FILE_ENTITY.remoteId.toString())
+
+    }
+
+
+    @Test
+    fun `getFolderContent returns a list of OCFile`() {
         every { dao.getFolderContent(any()) } returns listOf(DUMMY_FILE_ENTITY)
 
         val result = localDataSource.getFolderContent(DUMMY_FILE_ENTITY.id)
@@ -199,7 +264,7 @@ class OCLocalFileDataSourceTest {
     }
 
     @Test(expected = Exception::class)
-    fun `get folder content - ko`() {
+    fun `getFolderContent returns an exception`() {
         every { dao.getFolderContent(any()) } throws Exception()
 
         localDataSource.getFolderContent(DUMMY_FILE_ENTITY.id)
@@ -208,23 +273,217 @@ class OCLocalFileDataSourceTest {
     }
 
     @Test
-    fun `get folder images - ok`() {
+    fun `getSearchFolderContent returns a list of OCFile`() {
+        every { dao.getSearchFolderContent(any(), any()) } returns listOf(DUMMY_FILE_ENTITY)
+
+        val result = localDataSource.getSearchFolderContent(OC_FILE.id!!, "test")
+
+        assertEquals(listOf(OC_FILE) , result)
+
+        verify (exactly = 1) {
+            dao.getSearchFolderContent(DUMMY_FILE_ENTITY.id, "test")
+        }
+    }
+
+    @Test(expected = Exception::class)
+    fun `getSearchFolderContent returns an exception`() {
+        every { dao.getSearchFolderContent(any(), any()) } throws Exception()
+
+        localDataSource.getSearchFolderContent(OC_FILE.id!!, "test")
+
+    }
+
+    @Test
+    fun `getSearchAvailableOfflineFolderContent returns a list of OCFile`() {
+        every { dao.getSearchAvailableOfflineFolderContent(any(), any()) } returns listOf(DUMMY_FILE_ENTITY)
+
+        val result = localDataSource.getSearchAvailableOfflineFolderContent(OC_FILE.id!!, "test")
+
+        assertEquals(listOf(OC_FILE) , result)
+
+        verify (exactly = 1) {
+            dao.getSearchAvailableOfflineFolderContent(DUMMY_FILE_ENTITY.id, "test")
+        }
+    }
+
+    @Test(expected = Exception::class)
+    fun `getSearchAvailableOfflineFolderContent returns an exception`() {
+        every { dao.getSearchAvailableOfflineFolderContent(any(), any()) } throws Exception()
+
+        localDataSource.getSearchAvailableOfflineFolderContent(OC_FILE.id!!, "test")
+
+    }
+
+    @Test
+    fun `getSearchSharedByLinkFolderContent returns a list of OCFile`() {
+
+        every { dao.getSearchSharedByLinkFolderContent(any(), any()) } returns listOf(DUMMY_FILE_ENTITY)
+
+        val result = localDataSource.getSearchSharedByLinkFolderContent(OC_FILE.id!!, "test")
+
+        assertEquals(listOf(OC_FILE) , result)
+
+        verify (exactly = 1) {
+            dao.getSearchSharedByLinkFolderContent(DUMMY_FILE_ENTITY.id, "test")
+        }
+    }
+
+    @Test(expected = Exception::class)
+    fun `getSearchSharedByLinkFolderContent returns an exception`() {
+
+        every { dao.getSearchSharedByLinkFolderContent(any(), any()) }  throws Exception()
+
+        localDataSource.getSearchSharedByLinkFolderContent(OC_FILE.id!!, "test")
+
+    }
+
+    @Test
+    fun `getFolderContentWithSyncInfoAsFlow returns a flow of list OcFileAndFileSync`() = runBlocking {
+
+        every { dao.getFolderContentWithSyncInfoAsFlow(any()) } returns flowOf(listOf(ocFileAndFileSync))
+
+        val result = localDataSource.getFolderContentWithSyncInfoAsFlow(OC_FILE.id!!)
+
+        result.collect { result->
+            assertEquals(listOf(OC_FILE_WITH_SYNC_INFO_AND_SPACE) , result)
+        }
+
+        verify (exactly = 1) {
+            dao.getFolderContentWithSyncInfoAsFlow(OC_FILE.id!!)
+        }
+    }
+
+    @Test(expected = Exception::class)
+    fun `getFolderContentWithSyncInfoAsFlow returns an exception`() = runBlocking {
+
+        every { dao.getFolderContentWithSyncInfoAsFlow(any()) } throws Exception()
+
+        val result = localDataSource.getFolderContentWithSyncInfoAsFlow(OC_FILE.id!!)
+
+    }
+
+    @Test
+    fun `getFolderImages returns a list of OCFile`() {
         every { dao.getFolderByMimeType(any(), any()) } returns listOf(DUMMY_FILE_ENTITY)
 
         val result = localDataSource.getFolderImages(DUMMY_FILE_ENTITY.id)
 
         assertEquals(listOf(OC_FILE), result)
 
-        verify { dao.getFolderByMimeType(DUMMY_FILE_ENTITY.id, MIME_PREFIX_IMAGE) }
+        verify (exactly = 1) { dao.getFolderByMimeType(DUMMY_FILE_ENTITY.id, MIME_PREFIX_IMAGE) }
     }
 
     @Test(expected = Exception::class)
-    fun `get folder images - ko`() {
+    fun `getFolderImages returns a exception`() {
         every { dao.getFolderByMimeType(any(), any()) } throws Exception()
 
         localDataSource.getFolderImages(DUMMY_FILE_ENTITY.id)
 
-        verify { dao.getFolderByMimeType(DUMMY_FILE_ENTITY.id, MIME_PREFIX_IMAGE) }
+        verify (exactly = 1) { dao.getFolderByMimeType(DUMMY_FILE_ENTITY.id, MIME_PREFIX_IMAGE) }
+    }
+
+    @Test
+    fun `getSharedByLinkWithSyncInfoForAccountAsFlow returns a flow of list of OCFileWithSyncInfo`() = runBlocking {
+        every { dao.getFilesWithSyncInfoSharedByLinkAsFlow(any()) } returns flowOf(listOf(ocFileAndFileSync))
+
+        val result = localDataSource.getSharedByLinkWithSyncInfoForAccountAsFlow(DUMMY_FILE_ENTITY.owner)
+
+        result.collect { result ->
+            assertEquals(listOf(OC_FILE_WITH_SYNC_INFO_AND_SPACE), result)
+        }
+
+        verify (exactly = 1) {
+            dao.getFilesWithSyncInfoSharedByLinkAsFlow(DUMMY_FILE_ENTITY.owner)
+        }
+    }
+
+    @Test(expected = Exception::class)
+    fun `getSharedByLinkWithSyncInfoForAccountAsFlow returns a exception`() = runBlocking {
+        every { dao.getFilesWithSyncInfoSharedByLinkAsFlow(any()) } throws Exception()
+
+        val result = localDataSource.getSharedByLinkWithSyncInfoForAccountAsFlow(DUMMY_FILE_ENTITY.owner)
+
+        result.collect { result ->
+            assertEquals(listOf(OC_FILE_WITH_SYNC_INFO_AND_SPACE), result)
+        }
+    }
+
+    @Test
+    fun `getFilesWithSyncInfoAvailableOfflineFromAccountAsFlow returns a flow of list of OCFileWithSyncInfo`() = runBlocking {
+        every { dao.getFilesWithSyncInfoAvailableOfflineFromAccountAsFlow(any()) } returns flowOf(listOf(ocFileAndFileSync))
+
+        val result = localDataSource.getFilesWithSyncInfoAvailableOfflineFromAccountAsFlow(DUMMY_FILE_ENTITY.owner)
+
+        result.collect { result ->
+            assertEquals(listOf(OC_FILE_WITH_SYNC_INFO_AND_SPACE), result)
+        }
+
+        verify (exactly = 1) {
+            dao.getFilesWithSyncInfoAvailableOfflineFromAccountAsFlow(DUMMY_FILE_ENTITY.owner)
+        }
+    }
+
+    @Test(expected = Exception::class)
+    fun `getFilesWithSyncInfoAvailableOfflineFromAccountAsFlow returns an Exception`() = runBlocking {
+        every { dao.getFilesWithSyncInfoAvailableOfflineFromAccountAsFlow(any()) } throws Exception()
+
+        val result = localDataSource.getFilesWithSyncInfoAvailableOfflineFromAccountAsFlow(DUMMY_FILE_ENTITY.owner)
+
+    }
+
+    @Test
+    fun `getFilesAvailableOfflineFromAccount returns a flow of list of OCFile`() {
+        every { dao.getFilesAvailableOfflineFromAccount(any()) } returns listOf(OC_FILE.toEntity())
+
+        val result = localDataSource.getFilesAvailableOfflineFromAccount(DUMMY_FILE_ENTITY.owner)
+
+        assertEquals(listOf(OC_FILE), result)
+
+        verify (exactly = 1) {
+            dao.getFilesAvailableOfflineFromAccount(DUMMY_FILE_ENTITY.owner)
+        }
+    }
+
+    @Test(expected = Exception::class)
+    fun `getFilesAvailableOfflineFromAccount returns an exception`() {
+        every { dao.getFilesAvailableOfflineFromAccount(any()) } throws Exception()
+
+        val result = localDataSource.getFilesAvailableOfflineFromAccount(DUMMY_FILE_ENTITY.owner)
+
+    }
+
+    @Test
+    fun `getFilesAvailableOfflineFromEveryAccount returns list of OCFile`() {
+        every { dao.getFilesAvailableOfflineFromEveryAccount() } returns listOf(OC_FILE.toEntity())
+
+        val result = localDataSource.getFilesAvailableOfflineFromEveryAccount()
+
+        assertEquals(listOf(OC_FILE), result)
+
+        verify (exactly = 1) {
+            dao.getFilesAvailableOfflineFromEveryAccount()
+        }
+    }
+
+    @Test(expected = Exception::class)
+    fun `getFilesAvailableOfflineFromEveryAccount returns an exception`() {
+        every { dao.getFilesAvailableOfflineFromEveryAccount() } throws Exception()
+
+        val result = localDataSource.getFilesAvailableOfflineFromEveryAccount()
+
+    }
+
+    @Test
+    fun `moveFile returns list of OCFile`() {
+        every { dao.getFilesAvailableOfflineFromEveryAccount() } returns listOf(OC_FILE.toEntity())
+
+        val result = localDataSource.getFilesAvailableOfflineFromEveryAccount()
+
+        assertEquals(listOf(OC_FILE), result)
+
+        verify (exactly = 1) {
+            dao.getFilesAvailableOfflineFromEveryAccount()
+        }
     }
 
     @Test
