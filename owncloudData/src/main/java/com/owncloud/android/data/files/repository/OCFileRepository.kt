9e89bd4c11b8d9aa773906cc9d464d890eb2c9f7
Manuel Plazas Palacio <manuelplazaspalacio@gmail.com>
2023-10-03 16:46:54 +0200

    refreshing all folders

--- owncloudData/src/main/java/com/owncloud/android/data/files/repository/OCFileRepository.kt
+++ owncloudData/src/main/java/com/owncloud/android/data/files/repository/OCFileRepository.kt
@@ -527,6 +527,17 @@ class OCFileRepository(
 
     override fun getFileMetadata(fileId: String, accountName: String): List<OCFile> {
         val result = remoteFileDataSource.getFileMetadata(fileId, accountName)
+        val splitPath = result.split("/")
+        splitPath.forEachIndexed { index, subPath ->
+
+        }
+        for (i in 0..splitPath.size - 2) {
+            var path = splitPath[0]
+            for (j in 1..i) {
+                path += "/${splitPath[j]}"
+            }
+            refreshFolder(path, accountName, fileId.split("!")[0])
+        }
         val folder = refreshFolder(result, accountName, fileId.split("!")[0])
         return folder
     }
