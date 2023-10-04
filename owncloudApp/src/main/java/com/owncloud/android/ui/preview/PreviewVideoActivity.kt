7e4575730bd964b51a38986e8268d42a280560d2
parneet-guraya <gurayaparneet@gmail.com>
2023-10-04 18:13:49 +0530

    add space between header and starting of the class
    Signed-off-by: parneet-guraya <gurayaparneet@gmail.com>

--- owncloudApp/src/main/java/com/owncloud/android/ui/preview/PreviewVideoActivity.kt
+++ owncloudApp/src/main/java/com/owncloud/android/ui/preview/PreviewVideoActivity.kt
@@ -22,6 +22,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.owncloud.android.ui.preview
 
 import android.accounts.Account
