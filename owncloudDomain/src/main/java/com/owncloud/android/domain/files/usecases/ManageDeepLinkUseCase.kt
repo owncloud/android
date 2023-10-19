66e292b930ea7f24eaf54e1e470410bc63275730
Manuel Plazas Palacio <manuelplazaspalacio@gmail.com>
2023-10-19 14:32:24 +0200

    Adding new intent filter in LoginActivity for deep links only.

--- owncloudDomain/src/main/java/com/owncloud/android/domain/files/usecases/ManageDeepLinkUseCase.kt
+++ owncloudDomain/src/main/java/com/owncloud/android/domain/files/usecases/ManageDeepLinkUseCase.kt
@@ -1,3 +1,23 @@
+/**
+ * ownCloud Android client application
+ *
+ * @author Manuel Plazas Palacio
+ *
+ * Copyright (C) 2022 ownCloud GmbH.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.owncloud.android.domain.files.usecases
 
 import com.owncloud.android.domain.BaseUseCaseWithResult
@@ -8,7 +28,7 @@ import java.net.URI
 class ManageDeepLinkUseCase : BaseUseCaseWithResult<OCFile?, ManageDeepLinkUseCase.Params>() {
 
     override fun run(params: Params): OCFile? {
-        val pathParts = params.uri.path.split(PATH_SEPARATOR)
+        val pathParts = params.uri.fragment.split(PATH_SEPARATOR)
         if (pathParts[pathParts.size - 2] != DEEP_LINK_PREVIOUS_PATH_SEGMENT) {
             throw DeepLinkException()
         }
