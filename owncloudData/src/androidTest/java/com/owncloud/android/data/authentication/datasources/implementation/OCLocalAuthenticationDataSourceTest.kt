af84115eb5972d716ec4bf543cd57ca67a5020e1
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2023-11-07 15:18:22 +0100

    Changes in OCLocalAuthenticationDataSource unit tests

--- owncloudData/src/androidTest/java/com/owncloud/android/data/authentication/datasources/implementation/OCLocalAuthenticationDataSourceTest.kt
+++ owncloudData/src/androidTest/java/com/owncloud/android/data/authentication/datasources/implementation/OCLocalAuthenticationDataSourceTest.kt
@@ -2,7 +2,9 @@
  * ownCloud Android client application
  *
  * @author David González Verdugo
- * Copyright (C) 2020 ownCloud GmbH.
+ * @author Juan Carlos Garrote Gascón
+ *
+ * Copyright (C) 2023 ownCloud GmbH.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -21,8 +23,6 @@ package com.owncloud.android.data.authentication.datasources.implementation
 
 import android.accounts.Account
 import android.accounts.AccountManager
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.test.filters.MediumTest
 import androidx.test.platform.app.InstrumentationRegistry
 import com.owncloud.android.data.authentication.KEY_CLIENT_REGISTRATION_CLIENT_EXPIRATION_DATE
 import com.owncloud.android.data.authentication.KEY_CLIENT_REGISTRATION_CLIENT_ID
@@ -58,23 +58,17 @@ import com.owncloud.android.testutil.OC_USER_INFO
 import com.owncloud.android.testutil.oauth.OC_CLIENT_REGISTRATION
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.spyk
 import io.mockk.verify
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 
-@MediumTest
 class OCLocalAuthenticationDataSourceTest {
-    @Rule
-    @JvmField
-    val instantExecutorRule = InstantTaskExecutorRule()
 
     private lateinit var ocLocalAuthenticationDataSource: OCLocalAuthenticationDataSource
     private val accountManager = mockk<AccountManager>(relaxUnitFun = true)
-    private val preferencesProvider = spyk<SharedPreferencesProvider>()
+    private val preferencesProvider = mockk<SharedPreferencesProvider>(relaxUnitFun = true)
 
     @Before
     fun setUp() {
@@ -86,13 +80,7 @@ class OCLocalAuthenticationDataSourceTest {
             preferencesProvider,
             OC_ACCOUNT.type
         )
-        getAccountsByType(OC_ACCOUNT.type, arrayOf(OC_ACCOUNT))
-    }
-
-    private fun getAccountsByType(accountType: String, accounts: Array<Account>) {
-        every {
-            accountManager.getAccountsByType(accountType)
-        } returns accounts
+        mockGetAccountsByType(OC_ACCOUNT.type, arrayOf(OC_ACCOUNT))
     }
 
     @Test
@@ -111,13 +99,12 @@ class OCLocalAuthenticationDataSourceTest {
 
         val newAccount = Account(OC_ACCOUNT_NAME, OC_ACCOUNT.type)
 
+        assertEquals(newAccount.name, newAccountName)
+
         // One for checking if the account exists and another one for getting the new account
         verifyAccountsByTypeAreGot(newAccount.type, 2)
-
         verifyAccountIsExplicitlyAdded(newAccount, "password", 1)
         verifyAccountInfoIsUpdated(newAccount, OC_SECURE_SERVER_INFO_BASIC_AUTH, OC_USER_INFO, 1)
-
-        assertEquals(newAccount.name, newAccountName)
     }
 
     @Test(expected = AccountNotNewException::class)
@@ -138,7 +125,7 @@ class OCLocalAuthenticationDataSourceTest {
 
         mockSelectedAccountNameInPreferences()
 
-        ocLocalAuthenticationDataSource.addBasicAccount(
+        val accountName = ocLocalAuthenticationDataSource.addBasicAccount(
             OC_ACCOUNT_ID,
             OC_REDIRECTION_PATH.lastPermanentLocation,
             "password",
@@ -146,6 +133,8 @@ class OCLocalAuthenticationDataSourceTest {
             OC_USER_INFO.copy(id = OC_ACCOUNT_ID),
             OC_ACCOUNT_NAME
         )
+
+        assertEquals(OC_ACCOUNT_NAME, accountName)
 
         // One for getting account to update
         verifyAccountsByTypeAreGot(OC_ACCOUNT.type, 1)
@@ -236,7 +225,7 @@ class OCLocalAuthenticationDataSourceTest {
 
         mockSelectedAccountNameInPreferences()
 
-        ocLocalAuthenticationDataSource.addOAuthAccount(
+        val accountName = ocLocalAuthenticationDataSource.addOAuthAccount(
             OC_ACCOUNT_ID,
             OC_REDIRECTION_PATH.lastPermanentLocation,
             OC_AUTH_TOKEN_TYPE,
@@ -248,6 +237,8 @@ class OCLocalAuthenticationDataSourceTest {
             OC_ACCOUNT_NAME,
             OC_CLIENT_REGISTRATION
         )
+
+        assertEquals(OC_ACCOUNT_NAME, accountName)
 
         // One for getting account to update
         verifyAccountsByTypeAreGot(OC_ACCOUNT.type, 1)
@@ -299,7 +290,7 @@ class OCLocalAuthenticationDataSourceTest {
     }
 
     @Test
-    fun supportsOAuthOk() {
+    fun supportsOAuth2Ok() {
 
         every {
             accountManager.getUserData(OC_ACCOUNT, KEY_SUPPORTS_OAUTH2)
@@ -307,15 +298,15 @@ class OCLocalAuthenticationDataSourceTest {
 
         val supportsOAuth2 = ocLocalAuthenticationDataSource.supportsOAuth2(OC_ACCOUNT.name)
 
+        assertTrue(supportsOAuth2)
+
         verifyAccountsByTypeAreGot(OC_ACCOUNT.type, 1)
         verifyUserDataIsGot(OC_ACCOUNT, KEY_SUPPORTS_OAUTH2, 1)
-
-        assertEquals(true, supportsOAuth2)
     }
 
     @Test(expected = AccountNotFoundException::class)
     fun supportsOAuthAccountNotFound() {
-        getAccountsByType(OC_ACCOUNT.type, arrayOf())// That account does not exist
+        mockGetAccountsByType(OC_ACCOUNT.type, arrayOf()) // That account does not exist
 
         ocLocalAuthenticationDataSource.supportsOAuth2(OC_ACCOUNT.name)
     }
@@ -329,17 +320,23 @@ class OCLocalAuthenticationDataSourceTest {
 
         val baseUrl = ocLocalAuthenticationDataSource.getBaseUrl(OC_ACCOUNT.name)
 
+        assertEquals(OC_SECURE_BASE_URL, baseUrl)
+
         verifyAccountsByTypeAreGot(OC_ACCOUNT.type, 1)
         verifyUserDataIsGot(OC_ACCOUNT, KEY_OC_BASE_URL, 1)
-
-        assertEquals(OC_SECURE_BASE_URL, baseUrl)
     }
 
     @Test(expected = AccountNotFoundException::class)
     fun getBaseUrlAccountNotFound() {
-        getAccountsByType(OC_ACCOUNT.type, arrayOf()) // That account does not exist
+        mockGetAccountsByType(OC_ACCOUNT.type, arrayOf()) // That account does not exist
 
         ocLocalAuthenticationDataSource.getBaseUrl(OC_ACCOUNT.name)
+    }
+
+    private fun mockGetAccountsByType(accountType: String, accounts: Array<Account>) {
+        every {
+            accountManager.getAccountsByType(accountType)
+        } returns accounts
     }
 
     private fun mockSelectedAccountNameInPreferences(
@@ -348,24 +345,16 @@ class OCLocalAuthenticationDataSourceTest {
         every {
             preferencesProvider.getString(SELECTED_ACCOUNT, any())
         } returns selectedAccountName
-        every {
-            preferencesProvider.putString(any(), any())
-        } returns Unit
     }
 
     private fun mockRegularAccountCreationFlow() {
         // Step 1: Get accounts to know if the current account exists
-        getAccountsByType(OC_ACCOUNT.type, arrayOf()) // There's no accounts yet
+        mockGetAccountsByType(OC_ACCOUNT.type, arrayOf()) // There's no accounts yet
 
         // Step 2: Add new account
         every {
             accountManager.addAccountExplicitly(any(), any(), any())
         } returns true
-
-        // Step 3: Update just created account
-        every {
-            accountManager.setUserData(any(), any(), any())
-        } returns Unit
     }
 
     private fun verifyAccountsByTypeAreGot(accountType: String, exactly: Int) {
