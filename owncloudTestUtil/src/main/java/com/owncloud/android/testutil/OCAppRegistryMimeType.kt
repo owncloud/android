9a5d28c2353e841b5fc41e0b59a0ed82f7dc0ac8
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2023-11-07 09:20:07 +0100

    Changes in OCLocalAppRegistryDataSource unit tests

--- owncloudTestUtil/src/main/java/com/owncloud/android/testutil/OCAppRegistryMimeType.kt
+++ owncloudTestUtil/src/main/java/com/owncloud/android/testutil/OCAppRegistryMimeType.kt
@@ -1,11 +1,24 @@
 package com.owncloud.android.testutil
 
+import com.owncloud.android.data.appregistry.db.AppRegistryEntity
 import com.owncloud.android.domain.appregistry.model.AppRegistryMimeType
 
 val OC_APP_REGISTRY_MIMETYPE = AppRegistryMimeType(
     mimeType = "DIR",
     ext = "appRegistryMimeTypes.ext",
     appProviders = emptyList(),
+    name = "appRegistryMimeTypes.name",
+    icon = "appRegistryMimeTypes.icon",
+    description = "appRegistryMimeTypes.description",
+    allowCreation = true,
+    defaultApplication = "appRegistryMimeTypes.defaultApplication",
+)
+
+val OC_APP_REGISTRY_ENTITY = AppRegistryEntity(
+    accountName = OC_ACCOUNT_NAME,
+    mimeType = "DIR",
+    ext = "appRegistryMimeTypes.ext",
+    appProviders = "[]",
     name = "appRegistryMimeTypes.name",
     icon = "appRegistryMimeTypes.icon",
     description = "appRegistryMimeTypes.description",
