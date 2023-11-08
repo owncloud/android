1e18bc1397f63b569ce4c0228db4e2d741071eec
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2023-11-08 11:29:11 +0100

    Changes in OCRemoteCapabilitiesDataSource unit tests

--- owncloudData/src/test/java/com/owncloud/android/data/capabilities/datasources/implementation/OCRemoteCapabilitiesDataSourceTest.kt
+++ owncloudData/src/test/java/com/owncloud/android/data/capabilities/datasources/implementation/OCRemoteCapabilitiesDataSourceTest.kt
@@ -4,6 +4,7 @@
  * @author David González Verdugo
  * @author Jesús Recio
  * @author Aitor Ballesteros Pavón
+ * @author Juan Carlos Garrote Gascón
  *
  * Copyright (C) 2023 ownCloud GmbH.
  *
@@ -32,11 +33,11 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
 
 class OCRemoteCapabilitiesDataSourceTest {
+
     private lateinit var ocRemoteCapabilitiesDataSource: OCRemoteCapabilitiesDataSource
 
     private val ocCapabilityService: OCCapabilityService = mockk()
@@ -45,7 +46,7 @@ class OCRemoteCapabilitiesDataSourceTest {
 
     @Before
     fun setUp() {
-        every { clientManager.getCapabilityService(any()) } returns ocCapabilityService
+        every { clientManager.getCapabilityService(OC_ACCOUNT_NAME) } returns ocCapabilityService
 
         ocRemoteCapabilitiesDataSource =
             OCRemoteCapabilitiesDataSource(
@@ -55,26 +56,19 @@ class OCRemoteCapabilitiesDataSourceTest {
     }
 
     @Test
-    fun readRemoteCapabilities() {
-        val accountName = OC_ACCOUNT_NAME
-
+    fun `getCapabilities returns a OCCapability`() {
         val remoteCapability = remoteCapabilityMapper.toRemote(OC_CAPABILITY)!!
 
         val getRemoteCapabilitiesOperationResult = createRemoteOperationResultMock(remoteCapability, true)
 
         every { ocCapabilityService.getCapabilities() } returns getRemoteCapabilitiesOperationResult
 
-        // Get capability from remote datasource
-        val capabilities = ocRemoteCapabilitiesDataSource.getCapabilities(accountName)
+        val result = ocRemoteCapabilitiesDataSource.getCapabilities(OC_ACCOUNT_NAME)
 
-        assertNotNull(capabilities)
-
-        assertEquals(OC_CAPABILITY.accountName, capabilities.accountName)
-        assertEquals(OC_CAPABILITY.versionMajor, capabilities.versionMajor)
-        assertEquals(OC_CAPABILITY.versionMinor, capabilities.versionMinor)
-        assertEquals(OC_CAPABILITY.versionMicro, capabilities.versionMicro)
+        assertEquals(OC_CAPABILITY, result)
 
         verify(exactly = 1) {
+            clientManager.getCapabilityService(OC_ACCOUNT_NAME)
             ocCapabilityService.getCapabilities()
         }
     }
