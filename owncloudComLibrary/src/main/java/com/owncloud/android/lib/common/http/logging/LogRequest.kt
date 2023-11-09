bcdabdf079d375d11f51e0d85c8b31564404202c
Manuel Plazas Palacio <manuelplazaspalacio@gmail.com>
2023-11-09 13:44:16 +0100

    Changing http log request and response.

--- owncloudComLibrary/src/main/java/com/owncloud/android/lib/common/http/logging/LogRequest.kt
+++ owncloudComLibrary/src/main/java/com/owncloud/android/lib/common/http/logging/LogRequest.kt
@@ -1,11 +1,17 @@
 package com.owncloud.android.lib.common.http.logging
 
 data class LogRequest(
-    val requestId: String,
+    val request: Request
+)
+
+data class Request(
+    val body: String,
+    val headers: Map<String, String>,
+    val info: RequestInfo,
+)
+
+data class RequestInfo(
+    val id: String,
     val method: String,
     val url: String,
-    val headers: Map<String, String>,
-    val bodyLength: Long,
-    val bodyContentType: String,
-    val body: String,
 )
