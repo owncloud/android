afb4814f6dba75b904a37219ee6ac4e13ef99eb4
Manuel Plazas Palacio <manuelplazaspalacio@gmail.com>
2023-11-09 13:44:16 +0100

    Changing http log request and response.

--- owncloudComLibrary/src/main/java/com/owncloud/android/lib/common/http/logging/LogResponse.kt
+++ owncloudComLibrary/src/main/java/com/owncloud/android/lib/common/http/logging/LogResponse.kt
@@ -1,12 +1,29 @@
 package com.owncloud.android.lib.common.http.logging
 
 data class LogResponse(
-    val requestId: String,
-    val method: String,
-    val url: String,
-    val code: Int,
-    val headers: Map<String, String>,
-    val bodyLength: Int,
-    val bodyContentType: String,
-    val body: String,
+    val response: Response
 )
+
+data class Response(
+    val body: String,
+    val headers: Map<String, String>,
+    val info: ResponseInfo,
+)
+
+data class ResponseInfo(
+    val id: String,
+    val method: String,
+    val reply: Reply,
+    val url: String,
+    val bodyLength: Int,
+)
+
+data class Reply(
+    val cached: Boolean,
+    val duration: Long,
+    val durationString: String,
+    val status: Int,
+    val version: String
+)
+
+const val DURATION_FORMAT = "duration(%dh, %dmin, %ds, %dms)"
