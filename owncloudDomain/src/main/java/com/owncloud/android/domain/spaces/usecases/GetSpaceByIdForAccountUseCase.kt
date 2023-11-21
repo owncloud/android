b1b26167c28089ed30dc4c6e37563c60ba58ea63
Aitorbp <aitor.ballesteros.pavon@gmail.com>
2023-11-21 15:54:02 +0000

    cr - changed getSpaceById to getSpaceByIdForAccount

--- owncloudDomain/src/main/java/com/owncloud/android/domain/spaces/usecases/GetSpaceByIdForAccountUseCase.kt
+++ owncloudDomain/src/main/java/com/owncloud/android/domain/spaces/usecases/GetSpaceByIdForAccountUseCase.kt
@@ -23,10 +23,15 @@ import com.owncloud.android.domain.BaseUseCase
 import com.owncloud.android.domain.spaces.SpacesRepository
 import com.owncloud.android.domain.spaces.model.OCSpace
 
-class GetSpaceByIdUseCase(
+class GetSpaceByIdForAccountUseCase(
     private val spacesRepository: SpacesRepository
-) : BaseUseCase<OCSpace?, GetSpaceByIdUseCase.Params>() {
-    override fun run(params: Params) = spacesRepository.getSpaceById(params.accountName, params.spaceId)
+) : BaseUseCase<OCSpace?, GetSpaceByIdForAccountUseCase.Params>() {
+
+    override fun run(params: Params) = spacesRepository.getSpaceByIdForAccount(
+        spaceId = params.spaceId,
+        accountName = params.accountName
+    )
+
     data class Params(
         val accountName: String,
         val spaceId: String?,
