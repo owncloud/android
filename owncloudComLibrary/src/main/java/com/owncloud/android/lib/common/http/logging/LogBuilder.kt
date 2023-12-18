81344f205aee71fee359ad5a970950b667e34725
Aitorbp <aitor.ballesteros.pavon@gmail.com>
2023-12-18 10:52:36 +0000

    Changed initial named log and fix when type-content is jrd-json

--- owncloudComLibrary/src/main/java/com/owncloud/android/lib/common/http/logging/LogBuilder.kt
+++ owncloudComLibrary/src/main/java/com/owncloud/android/lib/common/http/logging/LogBuilder.kt
@@ -22,6 +22,7 @@
  */
 package com.owncloud.android.lib.common.http.logging
 
+import com.owncloud.android.lib.common.http.HttpConstants.CONTENT_TYPE_JRD_JSON
 import com.owncloud.android.lib.common.http.HttpConstants.CONTENT_TYPE_JSON
 import com.owncloud.android.lib.common.http.HttpConstants.CONTENT_TYPE_WWW_FORM
 import com.owncloud.android.lib.common.http.HttpConstants.CONTENT_TYPE_XML
@@ -38,5 +39,6 @@ fun MediaType?.isLoggable(): Boolean =
         (mediaType.type == "text" ||
                 mediaTypeString.contains(CONTENT_TYPE_XML) ||
                 mediaTypeString.contains(CONTENT_TYPE_JSON) ||
-                mediaTypeString.contains(CONTENT_TYPE_WWW_FORM))
+                mediaTypeString.contains(CONTENT_TYPE_WWW_FORM) ||
+                mediaTypeString.contains(CONTENT_TYPE_JRD_JSON))
     } ?: false
