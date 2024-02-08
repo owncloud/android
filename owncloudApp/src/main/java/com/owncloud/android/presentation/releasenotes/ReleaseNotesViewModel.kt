3d7906a1d7f0fe013773fb211a9f2791f7708e5a
Aitorbp <aitor.ballesteros.pavon@gmail.com>
2024-02-08 07:37:48 +0000

    Added new contributions issues to calens and changed release notes

--- owncloudApp/src/main/java/com/owncloud/android/presentation/releasenotes/ReleaseNotesViewModel.kt
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/releasenotes/ReleaseNotesViewModel.kt
@@ -93,16 +93,11 @@ class ReleaseNotesViewModel(
             ReleaseNote(
                 title = R.string.release_notes_4_2_0_title_10,
                 subtitle = R.string.release_notes_4_2_0_subtitle_10,
-                type = ReleaseNoteType.CHANGE,
+                type = ReleaseNoteType.ENHANCEMENT,
             ),
             ReleaseNote(
                 title = R.string.release_notes_4_2_0_title_11,
                 subtitle = R.string.release_notes_4_2_0_subtitle_11,
-                type = ReleaseNoteType.ENHANCEMENT,
-            ),
-            ReleaseNote(
-                title = R.string.release_notes_4_2_0_title_12,
-                subtitle = R.string.release_notes_4_2_0_subtitle_12,
                 type = ReleaseNoteType.BUGFIX,
             ),
         )
