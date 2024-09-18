63c80c785ff458b795922b7d0339b3e8cc7d55b6
Aitorbp <aitor.ballesteros.pavon@gmail.com>
2024-09-18 16:25:17 +0200

    feat: added setTitle in SettingsActivity to avoid owncloud voice in the talkback

--- owncloudApp/src/main/java/com/owncloud/android/presentation/settings/SettingsActivity.kt
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/settings/SettingsActivity.kt
@@ -3,8 +3,9 @@
  *
  * @author Juan Carlos Garrote Gascón
  * @author David Crespo Ríos
+ * @author Aitor Ballesteros Pavón
  *
- * Copyright (C) 2021 ownCloud GmbH.
+ * Copyright (C) 2024 ownCloud GmbH.
  * <p>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -70,7 +71,7 @@ class SettingsActivity : AppCompatActivity() {
             is SettingsMoreFragment -> R.string.prefs_subsection_more
             else -> R.string.actionbar_settings
         }
-
+        setTitle(titleId)
         supportActionBar?.setTitle(titleId)
     }
 
