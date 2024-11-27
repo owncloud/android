74e8b18153ef157d9d0635c77bb64685ed523455
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2024-11-27 12:12:25 +0100

    feat: hack not to show space header in "Av. offline" tab

--- owncloudApp/src/main/java/com/owncloud/android/presentation/files/filelist/MainFileListFragment.kt
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/files/filelist/MainFileListFragment.kt
@@ -612,7 +612,7 @@ class MainFileListFragment : Fragment(),
 
             binding.spaceHeader.root.apply {
                 if ((fileListUiState.space?.isProject == true || fileListUiState.space?.isPersonal == true)
-                    && fileListUiState.folderToDisplay?.remotePath == ROOT_PATH) {
+                    && fileListUiState.folderToDisplay?.remotePath == ROOT_PATH && fileListUiState.fileListOption != FileListOption.AV_OFFLINE) {
                     isVisible = true
                     animate().translationY(0f).duration = 100
                 } else {
