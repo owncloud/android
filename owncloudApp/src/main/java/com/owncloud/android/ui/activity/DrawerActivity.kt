34533314fca7c7d2271f2dead65254d460807764
joragua <jorgeagurec@gmail.com>
2024-12-20 09:42:18 +0100

    feat: removed progress bar from drawer when the quota is unlimited

--- owncloudApp/src/main/java/com/owncloud/android/ui/activity/DrawerActivity.kt
+++ owncloudApp/src/main/java/com/owncloud/android/ui/activity/DrawerActivity.kt
@@ -336,11 +336,7 @@ abstract class DrawerActivity : ToolbarActivity() {
             }
 
             userQuota.available < 0 -> { // Pending, unknown or unlimited free storage
-                getAccountQuotaBar()?.apply {
-                    isVisible = true
-                    progress = 0
-                    progressTintList = ColorStateList.valueOf(resources.getColor(R.color.color_accent))
-                }
+                getAccountQuotaBar()?.visibility = View.GONE
                 getAccountQuotaText()?.text = String.format(
                     getString(R.string.drawer_unavailable_free_storage),
                     DisplayUtils.bytesToHumanReadable(userQuota.used, this, true)
