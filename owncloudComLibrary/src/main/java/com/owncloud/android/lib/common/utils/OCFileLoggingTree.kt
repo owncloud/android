fac94c6dd047ebd782e38df90286381e5bdab2d7
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2025-01-27 11:20:13 +0100

    refactor: added explicit Locale not to rely on default Locale, which can lead to potential bugs

--- owncloudComLibrary/src/main/java/com/owncloud/android/lib/common/utils/OCFileLoggingTree.kt
+++ owncloudComLibrary/src/main/java/com/owncloud/android/lib/common/utils/OCFileLoggingTree.kt
@@ -90,6 +90,7 @@ class OCFileLoggingTree(
     override fun createStackElementTag(element: StackTraceElement): String {
         if (newLogcat) {
             method = String.format(
+                Locale.US,
                 "%s.%s()",
                 // method is fully qualified only when class differs on filename otherwise it can be cropped on long lambda expressions
                 super.createStackElementTag(element)?.replaceFirst(element.fileName.takeWhile { it != '.' }, ""),
@@ -97,6 +98,7 @@ class OCFileLoggingTree(
             )
 
             codeIdentifier = String.format(
+                Locale.US,
                 "(%s:%d)",
                 element.fileName,
                 element.lineNumber // format ensures line numbers have at least 3 places to align consecutive output from the same file
@@ -104,6 +106,7 @@ class OCFileLoggingTree(
             return "(${element.fileName}:${element.lineNumber})"
         } else
             return String.format(
+                Locale.US,
                 "(%s:%d) %s.%s()",
                 element.fileName,
                 element.lineNumber, // format ensures line numbers have at least 3 places to align consecutive output from the same file
