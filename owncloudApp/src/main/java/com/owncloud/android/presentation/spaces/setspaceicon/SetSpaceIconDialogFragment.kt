44b9c6a871eb322af7c7a08d25ae8744f18473af
joragua <jorgeagurec@gmail.com>
2025-11-25 18:15:40 +0100

    feat: generate image file from emoji and store it in cache directory

--- owncloudApp/src/main/java/com/owncloud/android/presentation/spaces/setspaceicon/SetSpaceIconDialogFragment.kt
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/spaces/setspaceicon/SetSpaceIconDialogFragment.kt
@@ -20,12 +20,17 @@
 
 package com.owncloud.android.presentation.spaces.setspaceicon
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.owncloud.android.databinding.SetSpaceIconDialogBinding
+import java.io.File
 
 class SetSpaceIconDialogFragment : DialogFragment() {
     private var _binding: SetSpaceIconDialogBinding? = null
@@ -38,16 +43,44 @@ class SetSpaceIconDialogFragment : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.cancelSetSpaceIconButton.setOnClickListener { dialog?.dismiss() }
+        binding.apply {
+            cancelSetSpaceIconButton.setOnClickListener { dialog?.dismiss() }
+            emojiPicker.setOnEmojiPickedListener { emojiItem ->
+                val emojiFile = convertEmojiToImageFile(emojiItem.emoji)
+                dialog?.dismiss()
+            }
+        }
+    }
+
+    private fun convertEmojiToImageFile(emoji: String): File {
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            textSize = 250f
+            textAlign = Paint.Align.CENTER
+        }
+        val bitmap = Bitmap.createBitmap(ICON_WIDTH, ICON_HEIGHT, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+
+        val bounds = Rect()
+        paint.getTextBounds(emoji, 0, emoji.length, bounds)
+        val baseline = (ICON_HEIGHT / 2f) - bounds.exactCenterY()
+
+        canvas.drawText(emoji, (ICON_WIDTH / 2f), baseline, paint)
+
+        val file = File(requireContext().cacheDir, EMOJI_FILE_NAME)
+        file.outputStream().use { output ->
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, output)
+        }
+        return file
     }
 
     companion object {
+        private const val ICON_HEIGHT = 405
+        private const val ICON_WIDTH = 720
+        private const val EMOJI_FILE_NAME = "emoji.png"
 
-        fun newInstance(): SetSpaceIconDialogFragment {
-            val args = Bundle().apply { }
-            return SetSpaceIconDialogFragment().apply {
-                arguments = args
+        fun newInstance(): SetSpaceIconDialogFragment =
+            SetSpaceIconDialogFragment().apply {
+                arguments = Bundle()
             }
-        }
     }
 }
